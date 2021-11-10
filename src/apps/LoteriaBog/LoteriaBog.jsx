643bcc41334de09f8dd6c8621a90ced158248b6d
Hector Melgarejo <hector.agudelo@cumplimos.co>
2021-11-10 10:04:28 -0500

    09/11/2021 10:04

--- src/apps/LoteriaBog/LoteriaBog.jsx
+++ src/apps/LoteriaBog/LoteriaBog.jsx
@@ -6,12 +6,15 @@ import CashierLoteria from "./Roles/CashierLoteria";
 
 const LoteriaBog = () => {
   const { roleInfo } = useAuth();
+  console.log(roleInfo)
   return (
     <ProvideLoteria>
       <div className="w-full flex flex-col justify-center items-center">
         {roleInfo !== undefined && roleInfo !== null ? (
           roleInfo.role[0] === 1 ? (
+            <div>
             <AdminLoteria />
+            <CashierLoteria/></div>
           ) : (
             <CashierLoteria />
           )
