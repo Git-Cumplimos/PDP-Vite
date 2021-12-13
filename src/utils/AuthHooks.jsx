e100fb480ddad31d0d02107b50df2df4d777fd24
Nicholas David <nicholas.carreno@cumplimos.co>
2021-12-13 18:31:15 -0500

    refactor: make faster fetchings

--- src/utils/AuthHooks.jsx
+++ src/utils/AuthHooks.jsx
@@ -62,63 +62,41 @@ const infoTicket = async (id_trx, Tipo_operacion, ticket) => {
   } catch (err) {}
 };
 
-const fetchAwsAuth = async () => {
+const fetchDane = async (codigo_dane) => {
   try {
-    const uInfo = await Auth.currentUserInfo();
-    const uSession = await Auth.currentSession();
-    return { email: uInfo?.attributes?.email, uInfo, uSession };
-  } catch (err) {
-    logger.debug(err);
-    throw err;
-  }
+    const resp_ciudad = await fetchData(
+      urlCiudad_dane,
+      "GET",
+      {
+        c_digo_dane_del_municipio: codigo_dane,
+      },
+      {}
+    );
+    return resp_ciudad[0].municipio;
+  } catch (err) {}
 };
 
-const fetchSuserInfo = async (email) => {
+const fetchOficinaLoteria = async (id_comercio) => {
   try {
-    let roleObj = {};
-    const suserInfo = await fetchData(urlLog, "GET", { correo: email }, {});
-    roleObj = { ...suserInfo };
-
-    try {
-      const resp_ciudad = await fetchData(
-        urlCiudad_dane,
-        "GET",
-        {
-          c_digo_dane_del_municipio: suserInfo.codigo_dane,
-        },
-        {}
-      );
-      roleObj = {
-        ...roleObj,
-        ciudad: resp_ciudad[0].municipio,
+    const resp_cod = await fetchData(
+      urlCod_loteria_oficina,
+      "GET",
+      {
+        id_comercio: id_comercio,
+      },
+      {}
+    );
+    if (!("msg" in resp_cod)) {
+      return {
+        cod_oficina_lot: resp_cod.cod_oficina_lot,
+        cod_sucursal_lot: resp_cod.cod_sucursal_lot,
       };
-    } catch (err) {}
-
-    try {
-      const resp_cod = await fetchData(
-        urlCod_loteria_oficina,
-        "GET",
-        {
-          id_comercio: suserInfo.id_comercio,
-        },
-        {}
-      );
-      if (!("msg" in resp_cod)) {
-        roleObj = {
-          ...roleObj,
-          cod_oficina_lot: resp_cod.cod_oficina_lot,
-          cod_sucursal_lot: resp_cod.cod_sucursal_lot,
-        };
-      } else {
-        roleObj = {
-          ...roleObj,
-          cod_oficina_lot: "PPVIR",
-          cod_sucursal_lot: "00",
-        };
-      }
-    } catch (err) {}
-
-    return roleObj;
+    } else {
+      return {
+        cod_oficina_lot: "PPVIR",
+        cod_sucursal_lot: "00",
+      };
+    }
   } catch (err) {}
 };
 
@@ -145,88 +123,17 @@ const getPermissions = async (email = "") => {
       return { uAccess: [], pdpU: null };
     }
 
-    // Get group of the user
-    const user_group_res = await fetchData(
-      `${url_permissions}/users-groups`,
+    // Get user
+    const permissions = await fetchData(
+      `${url_permissions}/users-permissions`,
       "GET",
-      {
-        Users_uuid: uuid,
-      }
+      { uuid }
     );
-    if (!user_group_res?.status) {
-      throw new Error(user_group_res?.msg);
+    if (!permissions?.status) {
+      throw new Error(user_res?.msg);
     }
-    if (
-      !Array.isArray(user_group_res?.obj) ||
-      user_group_res?.obj.length === 0
-    ) {
-      return { uAccess: [], pdpU: null };
-    }
+    const userAccess = permissions?.obj;
 
-    const userAccess = [];
-
-    for (const group of user_group_res?.obj) {
-      const id_group = group.Groups_id_group ?? 0;
-      if (id_group !== 0) {
-        // Get roles of the group
-        const group_roles_res = await fetchData(
-          `${url_permissions}/groups-roles`,
-          "GET",
-          {
-            Groups_id_group: id_group,
-          }
-        );
-        if (!group_roles_res?.status) {
-          throw new Error(group_roles_res?.msg);
-        }
-        const gr_res = group_roles_res?.obj;
-        if (Array.isArray(gr_res) && gr_res.length > 0) {
-          for (const role of gr_res) {
-            const id_role = role.Roles_id_role ?? 0;
-            if (id_role !== 0) {
-              // Get permissions of the role
-              const role_permissions_res = await fetchData(
-                `${url_permissions}/roles-permissions`,
-                "GET",
-                {
-                  Roles_id_role: id_role,
-                }
-              );
-              if (!role_permissions_res?.status) {
-                throw new Error(role_permissions_res?.msg);
-              }
-              if (
-                Array.isArray(role_permissions_res?.obj) &&
-                role_permissions_res?.obj.length > 0
-              ) {
-                for (const permission of role_permissions_res?.obj) {
-                  const id_permission =
-                    permission.Permissions_id_permission ?? -1;
-                  userAccess.push({ id_permission });
-                  /* if (id_permission !== 0) {
-                    const permissions_res = await fetchData(
-                      `${url_permissions}/permissions`,
-                      "GET",
-                      {
-                        id_permission,
-                      }
-                    );
-                    if (!permissions_res?.status) {
-                      throw new Error(permissions_res?.msg);
-                    }
-                    const per_res = permissions_res?.obj?.results;
-                    if (Array.isArray(per_res) && per_res.length > 0) {
-                      const { id_permission, name_permission } = per_res;
-                      userAccess.push({ id_permission, name_permission });
-                    }
-                  } */
-                }
-              }
-            }
-          }
-        }
-      }
-    }
     if (userAccess.length === 0) {
       notifyError("User has no permissions in db");
       return { uAccess: [], pdpU: null };
@@ -272,7 +179,7 @@ export const useProvideAuth = () => {
 
   const [roleInfo, setRoleInfo] = useState(null);
 
-  const [quotaInfo, setQuotaInfo] = useState(null);
+  const [quotaInfo, setQuotaInfo] = useState({ quota: 0, comision: 0 });
 
   const [pdpUser, setPdpUser] = useState(null);
 
@@ -287,6 +194,22 @@ export const useProvideAuth = () => {
   const history = useHistory();
 
   const { state, pathname } = useLocation();
+
+  const signOut = useCallback(() => {
+    Auth.signOut()
+      .then(() => {
+        setSignedIn(false);
+        setCognitoUser(null);
+        setUserInfo(null);
+        setUserSession(null);
+        setRoleInfo(null);
+        setUserPermissions(null);
+        setPdpUser(null);
+        setQuotaInfo({ quota: 0, comision: 0 });
+        history.replace("/login");
+      })
+      .catch(() => {});
+  }, [history]);
 
   const getQuota = useCallback(() => {
     if (roleInfo?.id_comercio && roleInfo?.id_dispositivo) {
@@ -309,21 +232,59 @@ export const useProvideAuth = () => {
     }
   }, [roleInfo?.id_comercio, roleInfo?.id_dispositivo]);
 
-  const saveUserData = useCallback((user, email, uData, uSession) => {
-    setSignedIn(true);
-    setCognitoUser(user);
-    setUserInfo(uData);
-    setUserSession(uSession);
-    fetchSuserInfo(email)
-      .then((role) => setRoleInfo(role))
-      .catch(() => {});
-    getPermissions(email)
-      .then(({ uAccess, pdpU }) => {
-        setUserPermissions(uAccess);
-        setPdpUser(pdpU);
-      })
-      .catch(() => {});
-  }, []);
+  const saveUserData = useCallback(
+    (user) => {
+      // Set user vars
+      setSignedIn(true);
+      setCognitoUser(user);
+
+      Auth.currentUserInfo()
+        .then((uInfo) => {
+          setUserInfo(uInfo);
+          const email = uInfo?.attributes?.email;
+
+          // Fetch suser info
+          fetchData(urlLog, "GET", { correo: email }, {})
+            .then((suserInfo) => {
+              setRoleInfo({ ...suserInfo });
+              fetchDane(suserInfo.codigo_dane)
+                .then((ciudad) => {
+                  setRoleInfo((role) => {
+                    return { ...role, ciudad };
+                  });
+                })
+                .catch(() => {});
+              fetchOficinaLoteria(suserInfo.id_comercio)
+                .then((oficina) => {
+                  setRoleInfo((role) => {
+                    return { ...role, ...oficina };
+                  });
+                })
+                .catch(() => {});
+            })
+            .catch(() => {});
+
+          // Fetch user permissions
+          getPermissions(email)
+            .then(({ uAccess, pdpU }) => {
+              const { active } = pdpU;
+              if (!active) {
+                signOut();
+              } else {
+                setUserPermissions(uAccess);
+                setPdpUser(pdpU);
+              }
+            })
+            .catch(() => {});
+        })
+        .catch(() => {});
+
+      Auth.currentSession()
+        .then((uSession) => setUserSession(uSession))
+        .catch(() => {});
+    },
+    [signOut]
+  );
 
   const handleSetupTOTP = useCallback(
     async (user) => {
@@ -342,39 +303,30 @@ export const useProvideAuth = () => {
     [cognitoUser?.username]
   );
 
-  const checkUser = useCallback(async () => {
-    let user = null;
-    let email = null;
-    let uInfo = null;
-    let uSession = null;
-    try {
-      if (Auth.user === null || Auth.user === undefined) {
-        user = await Auth.currentAuthenticatedUser();
-        const {
-          email: _email,
-          uInfo: _uInfo,
-          uSession: _uSession,
-        } = await fetchAwsAuth();
-        email = _email;
-        uInfo = _uInfo;
-        uSession = _uSession;
-      } else {
-        user = Auth.user;
-        email = Auth.user?.attributes?.email;
-        const { uInfo: _uInfo, uSession: _uSession } = await fetchAwsAuth();
-        uInfo = _uInfo;
-        uSession = _uSession;
-      }
+  const checkUser = useCallback(() => {
+    if (Auth.user === null || Auth.user === undefined) {
+      Auth.currentAuthenticatedUser()
+        .then((user) => {
+          if (!user) {
+            setSignedIn(false);
+            setCognitoUser(null);
+            return;
+          }
+          saveUserData(user);
+        })
+        .catch((err) => {
+          setSignedIn(false);
+          setCognitoUser(null);
+          logger.debug(err);
+        });
+    } else {
+      const user = Auth.user;
       if (!user) {
         setSignedIn(false);
         setCognitoUser(null);
         return;
       }
-      saveUserData(user, email, uInfo, uSession);
-    } catch (err) {
-      setSignedIn(false);
-      setCognitoUser(null);
-      logger.debug(err);
+      saveUserData(user);
     }
   }, [saveUserData]);
 
@@ -486,9 +438,7 @@ export const useProvideAuth = () => {
           totp,
           cognitoUser.challengeName
         );
-        const { email, uInfo, uSession } = await fetchAwsAuth();
-
-        saveUserData(loggedUser, email, uInfo, uSession);
+        saveUserData(loggedUser);
         history.push(state?.from || pathname === "/login" ? "/" : pathname);
       } catch (err) {
         if (err.code === "NotAuthorizedException") {
@@ -499,17 +449,6 @@ export const useProvideAuth = () => {
     },
     [cognitoUser, history, state, pathname, saveUserData]
   );
-
-  const signOut = useCallback(() => {
-    Auth.signOut()
-      .then(() => {
-        setCognitoUser(null);
-        setSignedIn(false);
-        setRoleInfo({});
-        history.push("/login");
-      })
-      .catch(() => {});
-  }, [history]);
 
   const handlesetPreferredMFA = useCallback(
     async (totp) => {
