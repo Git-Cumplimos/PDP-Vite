d2684567055f0097d834a463b5fb696a5e61e525
Nicholas David <nicholas.carreno@cumplimos.co>
2021-12-13 08:06:43 -0500

    fix loading component

--- src/components/Base/SimpleLoading/SimpleLoading.jsx
+++ src/components/Base/SimpleLoading/SimpleLoading.jsx
@@ -2,6 +2,13 @@ import classes from "./SimpleLoading.module.css";
 
 const SimpleLoading = ({ show = false }) => {
   const { modal, modalContent } = classes;
+
+  if (show) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "auto";
+  }
+
   return show ? (
     <div className={modal}>
       <div className={modalContent}>
