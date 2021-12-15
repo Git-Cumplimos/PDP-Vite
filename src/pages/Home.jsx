b1790de71e1443a6b3ef9db3b30d785c911a3add
Nicholas David <nicholas.carreno@cumplimos.co>
2021-12-15 11:57:01 -0500

    Añadir imagen bucket

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
 import HNavbar from "../components/Base/HNavbar/HNavbar";
 import { useUrls } from "../utils/UrlsHooks";
 
+import AWS from "aws-sdk";
+
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import ColpensionesImg from "../assets/img/COLPENSIONES.jpg";
 import Banner2 from "../assets/img/BANNER2.jpg";
+// import { notifyError } from "../utils/notify";
+
+AWS.config.update({
+  accessKeyId: process.env.REACT_APP_accessKeyId,
+  secretAccessKey: process.env.REACT_APP_secretAccessKey,
+});
+
+const S3_BUCKET = process.env.REACT_APP_BUCKET_CMS;
+const REGION = process.env.REACT_APP_REGION;
+
+const bucket = new AWS.S3({
+  params: { Bucket: S3_BUCKET },
+  region: REGION,
+});
 
 const Home = () => {
   const { urlsPrivateApps } = useUrls();
@@ -15,11 +31,25 @@ const Home = () => {
   //   "maria.valero@puntodepago.com.co",
   // ]);
 
+  const [imgTry, setImgTry] = useState(null);
+
   const imgs = useMemo(() => {
     return [
       { name: "Colpensiones", url: ColpensionesImg },
       { name: "Punto de pago", url: Banner2 },
     ];
+  }, []);
+
+  useEffect(() => {
+    const params = {
+      Bucket: S3_BUCKET,
+      Key: "Presentacion.jpg",
+    };
+    bucket
+      .getObject(params, (err, data) => {
+        if (err) console.log(err, err.stack);
+        else setImgTry(data);
+      })
   }, []);
 
   return (
@@ -42,6 +72,14 @@ const Home = () => {
         })}
       </Carousel>
       <HNavbar links={urlsPrivateApps} isIcon />
+      <img
+        src={URL.createObjectURL(
+          new Blob([imgTry?.Body], { type: "image/png" } /* (1) */)
+        )}
+        alt="Img try"
+      />
+      {/* <pre>{JSON.stringify(imgTry?.Body)}</pre> */}
+      {/* {imgTry?.Body} */}
     </>
   );
 };
