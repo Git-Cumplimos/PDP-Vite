203960d7837537095e27b9d638e685ca2221f84a
EstebanHerediaCumplimos <esteban.heredia@cumplimos.co>
2022-01-12 20:05:22 -0500

    URL PROD

--- src/apps/LoteriaBog/components/ReportVentasForm/ReportVentasForm.jsx
+++ src/apps/LoteriaBog/components/ReportVentasForm/ReportVentasForm.jsx
@@ -25,71 +25,53 @@ function createCard(
   fracciones,
   val_venta,
   fisico,
-  fecha_venta) {
+  fecha_venta
+) {
   return {
     cod_distribuidor,
-  cod_sucursal,
-  sorteo,
-  cod_loteria,
-  num_billete,
-  serie,
-  fracciones,
-  val_venta,
-  fisico,
-  fecha_venta
+    cod_sucursal,
+    sorteo,
+    cod_loteria,
+    num_billete,
+    serie,
+    fracciones,
+    val_venta,
+    fisico,
+    fecha_venta,
   };
 }
-const url_reportVentas = `http://127.0.0.1:5000/reportes_ventas`
+const url_reportVentas = `${process.env.REACT_APP_URL_LOTO_VENTA_REPORTES}/reportes_ventas`;
 
-
-
-const ReportVentasForm = ({closeModal, oficina}) => {
-
-  const [fecha_ini, setFecha_ini] = useState('');
-  const [fecha_fin, setFecha_fin] = useState('');
+const ReportVentasForm = ({ closeModal, oficina }) => {
+  const [fecha_ini, setFecha_ini] = useState("");
+  const [fecha_fin, setFecha_fin] = useState("");
   const [disabledBtns, setDisabledBtns] = useState(true);
-  const [resp_report,setResp_report] = useState(null)
-  const [total,setTotal] = useState(null)
-  
+  const [resp_report, setResp_report] = useState(null);
+  const [total, setTotal] = useState(null);
 
-  const reportVentas = useCallback(async (fecha_ini,fecha_fin) => {
+  const reportVentas = useCallback(async (fecha_ini, fecha_fin) => {
     try {
-      const query= {fecha_ini:fecha_ini,
-                    fecha_fin:fecha_fin,}
-      if(oficina!==undefined){
-        
-          query.cod_distribuidor=oficina?.cod_oficina_lot;
-          query.cod_sucursal=oficina?.cod_sucursal_lot; 
-
+      const query = { fecha_ini: fecha_ini, fecha_fin: fecha_fin };
+      if (oficina !== undefined) {
+        query.cod_distribuidor = oficina?.cod_oficina_lot;
+        query.cod_sucursal = oficina?.cod_sucursal_lot;
       }
-      
-      const res = await fetchData(url_reportVentas, "GET", 
-        query 
-      );
-      
+
+      const res = await fetchData(url_reportVentas, "GET", query);
+
       return res;
     } catch (err) {
       console.error(err);
     }
   }, []);
- 
-  
-  
-
-  
-  
 
   //const { reportVentas } = useLoteria();
 
-
-  
- 
-
   const exportdata = (e) => {
     e.preventDefault();
-    setDisabledBtns(true)
+    setDisabledBtns(true);
     closeModal();
-    
+
     const rows = [];
 
     resp_report.map((row) => {
@@ -104,7 +86,7 @@ const ReportVentasForm = ({closeModal, oficina}) => {
           row.fracciones,
           row.val_venta,
           row.fisico,
-          row.fecha_venta,
+          row.fecha_venta
         )
       );
       return null;
@@ -126,27 +108,22 @@ const ReportVentasForm = ({closeModal, oficina}) => {
     if (rows.length > 0) {
       csvExporter.generateCsv(rows);
     }
-    setFecha_ini('')
-    setFecha_fin('')
-    setResp_report(null)
-    setTotal(null)
+    setFecha_ini("");
+    setFecha_fin("");
+    setResp_report(null);
+    setTotal(null);
     return null;
   };
 
-  console.log(resp_report,fecha_ini,fecha_fin)
+  console.log(resp_report, fecha_ini, fecha_fin);
   return (
     <>
-
       <div className="flex flex-col justify-center items-center mx-auto container">
-        <Form  onSubmit={exportdata} grid>
-            <div
-              className="flex flex-row justify-between text-lg font-medium grid"
-            >
-             Seleccione el rango de fechas para generar el reporte
-
-              
-            </div>
-            {/* <Input
+        <Form onSubmit={exportdata} grid>
+          <div className="flex flex-row justify-between text-lg font-medium grid">
+            Seleccione el rango de fechas para generar el reporte
+          </div>
+          {/* <Input
             id="numsorteo"
             label="Sorteo"
             type="search"
@@ -162,94 +139,91 @@ const ReportVentasForm = ({closeModal, oficina}) => {
                 }
             }}     
             /> */}
-            <Input
-              id="fecha_ini"
-              label="Fecha inicial"
-              type="date"
-              required='true'
-              value={fecha_ini}
-              onInput={(e) => {
-                setFecha_ini(e.target.value);
-                if(fecha_fin!==''){
-                  reportVentas(e.target.value,fecha_fin)
-                    .then((res) => {                                      
-                        if('msg' in res){
-                          notifyError(res.msg)
-                          setDisabledBtns(true)
-                        }
-                        else{
-                          console.log(res)
-                          setResp_report(res.data)
-                          setTotal(res.total)                          
-                          setDisabledBtns(false)
-                        }                     
-                                    
-                    })}
-              }}
-            />
-            <Input
-              id="fecha_fin"
-              label="Fecha final"
-              type="date"
-              required='true'
-              value={fecha_fin}
-              onInput={(e) => {
-                setFecha_fin(e.target.value);
-                if(fecha_ini!==''){
-                reportVentas(fecha_ini,e.target.value)
-                  .then((res) => {                                      
-                      if('msg' in res){
-                        notifyError(res.msg)
-                        setDisabledBtns(true)
-                      }
-                      else{
-                        console.log(res)
-                        setResp_report(res.data)
-                        setTotal(res.total)
-                        setDisabledBtns(false)
-                      }                     
-                                  
-                  })}
-                
-              }}
-            /> 
-            {total!==null?
+          <Input
+            id="fecha_ini"
+            label="Fecha inicial"
+            type="date"
+            required="true"
+            value={fecha_ini}
+            onInput={(e) => {
+              setFecha_ini(e.target.value);
+              if (fecha_fin !== "") {
+                reportVentas(e.target.value, fecha_fin).then((res) => {
+                  if ("msg" in res) {
+                    notifyError(res.msg);
+                    setDisabledBtns(true);
+                  } else {
+                    console.log(res);
+                    setResp_report(res.data);
+                    setTotal(res.total);
+                    setDisabledBtns(false);
+                  }
+                });
+              }
+            }}
+          />
+          <Input
+            id="fecha_fin"
+            label="Fecha final"
+            type="date"
+            required="true"
+            value={fecha_fin}
+            onInput={(e) => {
+              setFecha_fin(e.target.value);
+              if (fecha_ini !== "") {
+                reportVentas(fecha_ini, e.target.value).then((res) => {
+                  if ("msg" in res) {
+                    notifyError(res.msg);
+                    setDisabledBtns(true);
+                  } else {
+                    console.log(res);
+                    setResp_report(res.data);
+                    setTotal(res.total);
+                    setDisabledBtns(false);
+                  }
+                });
+              }
+            }}
+          />
+          {total !== null ? (
             <>
-            <Input
-            id="frac_venta"
-            label="Fracciones vendidas"
-            type="text"
-            required='true'
-            value={total.total_frac}
-            />
-            <Input
-            id="val_total"
-            label="Total ventas"
-            type="text"
-            required='true'
-            value={formatMoney.format(total.val_total)}
-            />
+              <Input
+                id="frac_venta"
+                label="Fracciones vendidas"
+                type="text"
+                required="true"
+                value={total.total_frac}
+              />
+              <Input
+                id="val_total"
+                label="Total ventas"
+                type="text"
+                required="true"
+                value={formatMoney.format(total.val_total)}
+              />
             </>
-            :""}
-                       
-            <ButtonBar>
-            <Button type="submit" disabled={disabledBtns}>Descargar</Button>
+          ) : (
+            ""
+          )}
+
+          <ButtonBar>
+            <Button type="submit" disabled={disabledBtns}>
+              Descargar
+            </Button>
             <Button
               type="button"
               onClick={() => {
                 closeModal();
-                setFecha_fin('');
-                setFecha_ini('');
-                setResp_report(null) 
-                setTotal(null)
-                setDisabledBtns(true)       
-              
+                setFecha_fin("");
+                setFecha_ini("");
+                setResp_report(null);
+                setTotal(null);
+                setDisabledBtns(true);
               }}
             >
               Cancelar
             </Button>
-            </ButtonBar>            
-        
+          </ButtonBar>
         </Form>
       </div>
     </>
