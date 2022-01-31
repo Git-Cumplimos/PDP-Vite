c327f0ccafc3a75575a0532eaab00e9cb48c6d3a
EstebanHerediaCumplimos <esteban.heredia@cumplimos.co>
2022-01-31 09:20:04 -0500

    FDLM APP

--- src/apps/FundacionMujer/FunMujer.jsx
+++ src/apps/FundacionMujer/FunMujer.jsx
@@ -1,24 +1,7 @@
-import FundacionWomen from "./components/RolesFundamujer/FundacionWomen";
-import { useAuth } from "../../hooks/AuthHooks";
-import Providefundamujer from "./components/Providefundamujer";
+import HNavbar from "../../components/Base/HNavbar/HNavbar";
 
-const FunMujer = () => {
-  const { roleInfo } = useAuth();
-  return (
-    <Providefundamujer>
-    <div className="w-full flex flex-col justify-center items-center">
-      {roleInfo !== undefined && roleInfo !== null ? (
-        roleInfo.role === 0 ? (
-          ""
-        ) : (
-          <FundacionWomen/>
-        )
-      ) : (
-        ""
-      )}
-    </div>
-  </Providefundamujer>
-  );
+const FunMujer = ({ subRoutes }) => {
+  return <HNavbar links={subRoutes} isIcon />;
 };
 
 export default FunMujer;
