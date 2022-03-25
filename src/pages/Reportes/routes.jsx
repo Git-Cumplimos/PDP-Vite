baed0fb38ee2e318138abf7d1f6797db6e779f02
Nicholas David <nicholas.carreno@cumplimos.co>
2022-03-25 11:56:27 -0500

    fix: permissions for production

--- src/pages/Reportes/routes.jsx
+++ src/pages/Reportes/routes.jsx
@@ -7,14 +7,14 @@ const ReporteComisiones = lazy(() => import("./views/ReporteComisiones"));
 export const rutasReportes = [
   {
     link: "/reportes/koncilia",
-    label: <AppIcons Logo={"RECAUDO"} name='Reportes koncilia' />,
+    label: <AppIcons Logo={"RECAUDO"} name="Reportes koncilia" />,
     component: Koncilia,
     permission: [40],
   },
   {
     link: "/reportes/comisiones",
-    label: <AppIcons Logo={"RECAUDO"} name='Reportes comisiones' />,
+    label: <AppIcons Logo={"RECAUDO"} name="Reportes comisiones" />,
     component: ReporteComisiones,
-    permission: [1],
+    permission: [42],
   },
 ];
