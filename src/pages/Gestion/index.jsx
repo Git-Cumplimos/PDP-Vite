3c1e6ae124364a8f9d18cc9198d2cd6d1f7bc03c
EstebanHerediaCumplimos <esteban.heredia@cumplimos.co>
2022-04-18 11:23:58 -0500

    last changes to index: gestion

--- src/pages/Gestion/index.jsx
+++ src/pages/Gestion/index.jsx
@@ -4,16 +4,7 @@ import ProvideGestion from "../menu/gestion/provider/ProvideGestion";
 
 const Gestion = () => {
   const { urlsGestion } = useUrls();
-<<<<<<< HEAD:src/pages/Gestion.jsx
-
-  return (
-    <ProvideGestion>
-      <HNavbar links={urlsGestion} isIcon />
-    </ProvideGestion>
-  );
-=======
   return <HNavbar links={urlsGestion} isIcon />;
->>>>>>> ac0e4af02d1e3407e60543e7eeab37793dc43120:src/pages/Gestion/index.jsx
 };
 
 export default Gestion;
