7ea9833b59da2e3ba8ce1eb14b629c1dccc0b0bd
Nicholas David <nicholas.carreno@cumplimos.co>
2022-05-25 18:18:12 -0500

    feat: update assets to use cloudfront endpoint

--- src/utils/AssetsObjects.js
+++ src/utils/AssetsObjects.js
@@ -1,59 +1,38 @@
+const urlAssets = process.env.REACT_APP_ASSETS_URL;
+
 const images = {
-  BANNER2: "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/img/BANNER2.jpg",
-  COLPENSIONES:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/img/COLPENSIONES.jpg",
-  pdpHorizontal:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/img/logosPuntoDePagoHorizontal.png",
-  pdpVertical:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/img/logosPuntoDePagoVertical.jpg",
-  Loteria_de_Bogota:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/img/Loteria_de_Bogota.png",
-  personas:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/img/personas.png",
+  BANNER2: `${urlAssets}/assets/img/BANNER2.jpg`,
+  COLPENSIONES: `${urlAssets}/assets/img/COLPENSIONES.jpg`,
+  pdpHorizontal: `${urlAssets}/assets/img/logosPuntoDePagoHorizontal.png`,
+  pdpVertical: `${urlAssets}/assets/img/logosPuntoDePagoVertical.jpg`,
+  Loteria_de_Bogota: `${urlAssets}/assets/img/Loteria_de_Bogota.png`,
+  personas: `${urlAssets}/assets/img/personas.png`,
 };
 
 const svgs = {
-  ACTUALIZACION:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/ActualizacionDeDatos.svg",
-  backIcon2:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/back-icon-2.svg",
-  backIconSecondary:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/back-icon-secondary.svg",
-  backIcon:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/back-icon.svg",
-  bar: "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/bar.svg",
-  BarThin: "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/BarThin.svg",
-  LOTERIA:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/LOTERIA-DE-BOGOTA-01.svg",
-  MARKETPLACE:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/MARKETPLACE-01.svg",
-  PAGO: "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/PAGO-01.svg",
-  REPORTE:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/REPORTES-01.svg",
-  right_arrow:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/right-arrow.svg",
-  SORTEOS:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/SORTEO-01.svg",
-  SUSER: "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/SUSER-01.svg",
+  ACTUALIZACION: `${urlAssets}/assets/svg/ActualizacionDeDatos.svg`,
+  backIcon2: `${urlAssets}/assets/svg/back-icon-2.svg`,
+  backIconSecondary: `${urlAssets}/assets/svg/back-icon-secondary.svg`,
+  backIcon: `${urlAssets}/assets/svg/back-icon.svg`,
+  bar: `${urlAssets}/assets/svg/bar.svg`,
+  BarThin: `${urlAssets}/assets/svg/BarThin.svg`,
+  LOTERIA: `${urlAssets}/assets/svg/LOTERIA-DE-BOGOTA-01.svg`,
+  MARKETPLACE: `${urlAssets}/assets/svg/MARKETPLACE-01.svg`,
+  PAGO: `${urlAssets}/assets/svg/PAGO-01.svg`,
+  REPORTE: `${urlAssets}/assets/svg/REPORTES-01.svg`,
+  right_arrow: `${urlAssets}/assets/svg/right-arrow.svg`,
+  SORTEOS: `${urlAssets}/assets/svg/SORTEO-01.svg`,
+  SUSER: `${urlAssets}/assets/svg/SUSER-01.svg`,
   // Recaudo
-  CATALOGOS_Y_REVISTAS:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/recaudo/CATALOGOS&REVISTAS.svg",
-  EPS_Y_SALUD:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/recaudo/EPS&SALUD.svg",
-  IMPUESTO:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/recaudo/IMPUESTO.svg",
-  PAGO_DE_SEGURIDAD_SOCIAL:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/recaudo/PAGO_DE_SEGURIDAD_SOCIAL.svg",
-  PRODUCTOS_FINANCIEROS:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/recaudo/PRODUCTOS_FINANCIEROS.svg",
-  RECAUDO:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/recaudo/RECAUDO.svg",
-  RETIRO:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/recaudo/RETIRO.svg",
-  SERVICIOS_PUBLICOS:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/recaudo/SERVICIOS_PUBLICOS.svg",
-  UNIVERSIDADES:
-    "https://cms-pdp.s3.us-east-2.amazonaws.com/assets/svg/recaudo/UNIVERSIDADES.svg",
+  CATALOGOS_Y_REVISTAS: `${urlAssets}/assets/svg/recaudo/CATALOGOS&REVISTAS.svg`,
+  EPS_Y_SALUD: `${urlAssets}/assets/svg/recaudo/EPS&SALUD.svg`,
+  IMPUESTO: `${urlAssets}/assets/svg/recaudo/IMPUESTO.svg`,
+  PAGO_DE_SEGURIDAD_SOCIAL: `${urlAssets}/assets/svg/recaudo/PAGO_DE_SEGURIDAD_SOCIAL.svg`,
+  PRODUCTOS_FINANCIEROS: `${urlAssets}/assets/svg/recaudo/PRODUCTOS_FINANCIEROS.svg`,
+  RECAUDO: `${urlAssets}/assets/svg/recaudo/RECAUDO.svg`,
+  RETIRO: `${urlAssets}/assets/svg/recaudo/RETIRO.svg`,
+  SERVICIOS_PUBLICOS: `${urlAssets}/assets/svg/recaudo/SERVICIOS_PUBLICOS.svg`,
+  UNIVERSIDADES: `${urlAssets}/assets/svg/recaudo/UNIVERSIDADES.svg`,
 };
 
 export { images, svgs };
