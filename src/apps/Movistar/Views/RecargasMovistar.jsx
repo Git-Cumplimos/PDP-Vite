a48a638b906b58aba350fcb09e2c1573e76f246f
Brenda Montero <brenda.montero@cumplimos.co>
2022-06-23 14:18:46 -0500

    modal, con errores

--- src/apps/Movistar/Views/RecargasMovistar.jsx
+++ src/apps/Movistar/Views/RecargasMovistar.jsx
@@ -6,48 +6,48 @@ import Input from "../../../components/Base/Input";
 import MoneyInput from "../../../components/Base/MoneyInput";
 import { useAuth } from "../../../hooks/AuthHooks";
 import { PeticionRecarga, RealizarPeticionPro } from "../utils/fetchMovistar";
-import Modal from "../../../components/Base/Modal";
-import PaymentSummary from "../../../components/Compound/PaymentSummary";
-import Tickets from "../../../components/Base/Tickets";
-import { useFetch } from "../../../hooks/useFetch";
-import fetchData from "../../../utils/fetchData";
+// import Modal from "../../../components/Base/Modal";
+// import PaymentSummary from "../../../components/Compound/PaymentSummary";
+// import Tickets from "../../../components/Base/Tickets";
+// import { useFetch } from "../../../hooks/useFetch";
+// import fetchData from "../../../utils/fetchData";
 
 const URL = "http://127.0.0.1:5000/recargasMovistar/prepago";
 
 const RecargasMovistar = () => {
   const [inputCelular, setInputCelular] = useState(null);
   const [inputValor, setInputValor] = useState(null);
-  const [showModal, setShowModal] = useState(false);
-  const [paymentStatus, setPaymentStatus] = useState(false);
+  // const [showModal, setShowModal] = useState(false);
+  // const [paymentStatus, setPaymentStatus] = useState(false);
  
-  const postCashIn = async (bodyObj) => {
-    if (!bodyObj) {
-      return new Promise((resolve, reject) => {
-        resolve("Sin datos body");
-      });
-    }
-    try {
-      const res = await fetchData(
-        `${URL}`,
-        "POST",
-        {},
-        bodyObj
-      );
-      if (!res?.status) {
-        console.error(res?.msg);
-      }
-      return res;
-    } catch (err) {
-      throw err;
-    }
-  }
-  const [loadingCashIn, fetchCashIn] = useFetch(postCashIn);
+  // const postCashIn = async (bodyObj) => {
+  //   if (!bodyObj) {
+  //     return new Promise((resolve, reject) => {
+  //       resolve("Sin datos body");
+  //     });
+  //   }
+  //   try {
+  //     const res = await fetchData(
+  //       `${URL}`,
+  //       "POST",
+  //       {},
+  //       bodyObj
+  //     );
+  //     if (!res?.status) {
+  //       console.error(res?.msg);
+  //     }
+  //     return res;
+  //   } catch (err) {
+  //     throw err;
+  //   }
+  // }
+  // const [loadingCashIn, fetchCashIn] = useFetch(postCashIn);
 
   
 
-  const handleClose = useCallback(() => {
-    setShowModal(false);
-  }, []);
+  // const handleClose = useCallback(() => {
+  //   setShowModal(false);
+  // }, []);
   const { roleInfo } = useAuth();
   const l = "";
   const onChange = useCallback((e) => {
@@ -106,7 +106,7 @@ const RecargasMovistar = () => {
           Realizar deposito
         </Button>
       </Form>
-      <Modal
+      {/* <Modal
         show={showModal}
         handleClose={
           paymentStatus ? () => {} : loadingCashIn ? () => {} : handleClose
@@ -137,7 +137,7 @@ const RecargasMovistar = () => {
           </PaymentSummary>
         )
         }
-      </Modal>
+      </Modal> */}
     </Fragment>
   );
 };
