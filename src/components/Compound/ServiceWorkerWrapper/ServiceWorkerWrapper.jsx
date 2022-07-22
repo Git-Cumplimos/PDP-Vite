bd935d9cea8c295991831b5c70b47ea0ee7d19b5
Nicholas David <nicholas.carreno@cumplimos.co>
2022-07-22 13:45:04 -0500

    fix: add button to load lastest version of the web app

---
+++ src/components/Compound/ServiceWorkerWrapper/ServiceWorkerWrapper.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect, Fragment } from "react";
+import { toast } from "react-toastify";
+
+import * as serviceWorkerRegistration from "../../../serviceWorkerRegistration";
+
+const ServiceWorkerWrapper = () => {
+  const [showReload, setShowReload] = useState(false);
+  const [waitingWorker, setWaitingWorker] = useState(null);
+
+  const onSWUpdate = (registration) => {
+    setShowReload(true);
+    setWaitingWorker(registration.waiting);
+  };
+
+  useEffect(() => {
+    serviceWorkerRegistration.register({ onUpdate: onSWUpdate });
+  }, []);
+
+  const reloadPage = () => {
+    waitingWorker?.postMessage({ type: "SKIP_WAITING" });
+    setShowReload(false);
+    window.location.reload(true);
+  };
+
+  if (showReload) {
+    toast.info(
+      <div className="grid grid-flow-col items-center">
+        <h1>Nueva version de la pagina disponible</h1>
+        <div>
+          <button
+            className="px-2 py-1 bg-primary text-white rounded text-sm"
+            color="inherit"
+            size="small"
+            onClick={reloadPage}
+          >
+            Recargar
+          </button>
+        </div>
+      </div>,
+      {
+        autoClose: false,
+        closeOnClick: false,
+      }
+    );
+  }
+
+  return <Fragment></Fragment>;
+};
+
+export default ServiceWorkerWrapper;
