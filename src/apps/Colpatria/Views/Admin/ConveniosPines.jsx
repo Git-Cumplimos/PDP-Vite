206fe386cf0e6178fe09951df72d45ef94515867
Nicholas David <nicholas.carreno@cumplimos.co>
2022-07-29 17:05:01 -0500

    feat: add modify function to conv pines

--- src/apps/Colpatria/Views/Admin/ConveniosPines.jsx
+++ src/apps/Colpatria/Views/Admin/ConveniosPines.jsx
@@ -12,6 +12,7 @@ import { notifyError, notifyPending } from "../../../../utils/notify";
 import {
   getConveniosPinesList,
   addConveniosPinesList,
+  modConveniosPinesList,
   getConveniosPinesTiposValores,
 } from "../../utils/fetchFunctions";
 
@@ -28,25 +29,7 @@ const ConveniosPines = () => {
 
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    getConveniosPinesTiposValores()
-      .then((res) =>
-        setTiposValores(
-          res?.obj?.map(({ pk_id_tipo_valor, nombre_tipo_valor }) => ({
-            label: nombre_tipo_valor,
-            valor: pk_id_tipo_valor,
-          }))
-        )
-      )
-      .catch((err) => {
-        if (err?.cause === "custom") {
-          notifyError(err?.message);
-          return;
-        }
-        console.error(err?.message);
-      });
-  }, []);
-  useEffect(() => {
+  const getConvPines = useCallback(() => {
     getConveniosPinesList({ ...pageData })
       .then((res) => {
         setListaConveniosPines(res?.obj?.results ?? []);
@@ -61,6 +44,28 @@ const ConveniosPines = () => {
       });
   }, [pageData]);
 
+  useEffect(() => {
+    getConveniosPinesTiposValores()
+      .then((res) =>
+        setTiposValores(
+          res?.obj?.map(({ pk_id_tipo_valor, nombre_tipo_valor }) => ({
+            label: nombre_tipo_valor,
+            value: pk_id_tipo_valor,
+          }))
+        )
+      )
+      .catch((err) => {
+        if (err?.cause === "custom") {
+          notifyError(err?.message);
+          return;
+        }
+        console.error(err?.message);
+      });
+  }, []);
+  useEffect(() => {
+    getConvPines();
+  }, [getConvPines]);
+
   const handleClose = useCallback(() => {
     if (!loading) {
       setShowModal(false);
@@ -69,36 +74,47 @@ const ConveniosPines = () => {
     }
   }, [loading]);
 
-  const addConvenio = useCallback((ev) => {
-    ev.preventDefault();
-    const formData = new FormData(ev.currentTarget);
-    const body = Object.fromEntries(formData);
-    console.log(body);
-    notifyPending(
-      addConveniosPinesList(body),
-      {
-        render() {
-          setLoading(true);
-          return "Enviando solicitud";
+  const handleConvenio = useCallback(
+    (ev) => {
+      ev.preventDefault();
+      const formData = new FormData(ev.currentTarget);
+      const body = Object.fromEntries(formData);
+      console.log(body);
+      notifyPending(
+        selected
+          ? modConveniosPinesList({ pk_codigo_convenio: "" }, body)
+          : addConveniosPinesList(body),
+        {
+          render() {
+            setLoading(true);
+            return "Enviando solicitud";
+          },
         },
-      },
-      {
-        render({ data: res }) {
-          console.log(res);
-          return "Convenio agregado exitosamente";
+        {
+          render({ data: res }) {
+            setLoading(false);
+            console.log(res);
+            handleClose();
+            getConvPines();
+            return `Convenio ${
+              selected ? "modificado" : "agregado"
+            } exitosamente`;
+          },
         },
-      },
-      {
-        render({ data: err }) {
-          if (err?.cause === "custom") {
-            return err?.message;
-          }
-          console.error(err?.message);
-          return "Creacion fallida";
-        },
-      }
-    );
-  }, []);
+        {
+          render({ data: err }) {
+            setLoading(false);
+            if (err?.cause === "custom") {
+              return err?.message;
+            }
+            console.error(err?.message);
+            return `${selected ? "Edicion" : "Creacion"} fallida`;
+          },
+        }
+      );
+    },
+    [handleClose, getConvPines, selected]
+  );
 
   return (
     <Fragment>
@@ -119,15 +135,19 @@ const ConveniosPines = () => {
       </ButtonBar>
       <TableEnterprise
         title="Convenios de pines"
-        headers={[
-          "Id error",
-          "Codigo de error",
-          "Codigo de error base 64",
-          "Mensaje de error",
-        ]}
+        headers={["Codigo convenio", "Codigo pin", "Nombre convenio", "Estado"]}
         data={listaConveniosPines.map(
-          ({ error_code, error_code_b64, error_msg, pk_error_id }, index) => ({
-            pk_error_id,
+          ({
+            pk_codigo_convenio,
+            codigo_pin,
+            nombre_convenio,
+            fk_tipo_valor,
+            activo,
+          }) => ({
+            pk_codigo_convenio,
+            codigo_pin,
+            nombre_convenio,
+            activo: activo ? "Activo" : "No activo",
           })
         )}
         maxPage={maxPages}
@@ -138,116 +158,122 @@ const ConveniosPines = () => {
         }}
       ></TableEnterprise>
       <Modal show={showModal} handleClose={handleClose}>
-        {!selected ? (
-          uploadMasivo ? (
-            <Form grid>
-              <FileInput
-                label={"Archivo masivo"}
-                onGetFile={() => {}}
-                allowDrop={false}
+        {uploadMasivo ? (
+          <Form grid>
+            <FileInput
+              label={"Archivo masivo"}
+              onGetFile={() => {}}
+              allowDrop={false}
+            />
+            <ButtonBar>
+              <Button type={"submit"}>Realizar carge</Button>
+            </ButtonBar>
+          </Form>
+        ) : (
+          <Fragment>
+            <h1 className="text-3xl mx-auto text-center mb-4">
+              {selected ? "Editar" : "Crear"} convenio
+            </h1>
+            <Form onSubmit={handleConvenio} grid>
+              <Input
+                id={"pk_codigo_convenio"}
+                label={"Codigo de convenio"}
+                name={"pk_codigo_convenio"}
+                type="tel"
+                autoComplete="off"
+                maxLength={"6"}
+                onChange={(ev) => {
+                  ev.target.value = onChangeNumber(ev);
+                }}
+                defaultValue={selected?.pk_codigo_convenio ?? ""}
+                // disabled={selected ?? false}
+                required
+              />
+              <Input
+                id={"codigo_pin"}
+                label={"Codigo pin"}
+                name={"codigo_pin"}
+                type="tel"
+                autoComplete="off"
+                maxLength={"4"}
+                onChange={(ev) => {
+                  ev.target.value = onChangeNumber(ev);
+                }}
+                defaultValue={selected?.codigo_pin ?? ""}
+                required
+              />
+              <Input
+                id={"nombre_convenio"}
+                label={"Nombre del Convenio"}
+                name={"nombre_convenio"}
+                type="text"
+                autoComplete="off"
+                maxLength={"255"}
+                defaultValue={selected?.nombre_convenio ?? ""}
+                required
+              />
+              <Select
+                className="place-self-stretch"
+                id={"fk_tipo_valor"}
+                label={"Modificar valor"}
+                name={"fk_tipo_valor"}
+                options={[{ label: "", value: "" }, ...tiposValores]}
+                defaultValue={selected?.fk_tipo_valor ?? ""}
+                required
+              />
+              <Input
+                id={"referencia_1"}
+                label={"Referencia 1"}
+                name={"referencia_1"}
+                type="text"
+                autoComplete="off"
+                maxLength={"255"}
+                defaultValue={selected?.referencia_1 ?? ""}
+                required
+              />
+              <Input
+                id={"referencia_2"}
+                label={"Referencia 2"}
+                name={"referencia_2"}
+                type="text"
+                autoComplete="off"
+                maxLength={"255"}
+                defaultValue={selected?.referencia_2 ?? ""}
+              />
+              <Input
+                id={"referencia_3"}
+                label={"Referencia 3"}
+                name={"referencia_3"}
+                type="text"
+                autoComplete="off"
+                maxLength={"255"}
+                defaultValue={selected?.referencia_3 ?? ""}
+              />
+              <Input
+                id={"referencia_4"}
+                label={"Referencia 4"}
+                name={"referencia_4"}
+                type="text"
+                autoComplete="off"
+                maxLength={"255"}
+                defaultValue={selected?.referencia_4 ?? ""}
+              />
+              <Input
+                id={"referencia_5"}
+                label={"Referencia 5"}
+                name={"referencia_5"}
+                type="text"
+                autoComplete="off"
+                maxLength={"255"}
+                defaultValue={selected?.referencia_5 ?? ""}
               />
               <ButtonBar>
-                <Button type={"submit"}>Realizar carge</Button>
+                <Button type={"submit"} disabled={loading}>
+                  {selected ? "Realizar cambios" : "Crear convenio pin"}
+                </Button>
               </ButtonBar>
             </Form>
-          ) : (
-            <Fragment>
-              <h1 className="text-3xl mx-auto text-center mb-4">
-                Crear convenio
-              </h1>
-              <Form onSubmit={addConvenio} grid>
-                <Input
-                  id={"pk_codigo_convenio"}
-                  label={"Codigo de convenio"}
-                  name={"pk_codigo_convenio"}
-                  type="tel"
-                  autoComplete="off"
-                  maxLength={"6"}
-                  onChange={(ev) => {
-                    ev.target.value = onChangeNumber(ev);
-                  }}
-                  required
-                />
-                <Input
-                  id={"codigo_pin"}
-                  label={"Codigo pin"}
-                  name={"codigo_pin"}
-                  type="tel"
-                  autoComplete="off"
-                  maxLength={"4"}
-                  onChange={(ev) => {
-                    ev.target.value = onChangeNumber(ev);
-                  }}
-                  required
-                />
-                <Input
-                  id={"nombre_convenio"}
-                  label={"Nombre del Convenio"}
-                  name={"nombre_convenio"}
-                  type="text"
-                  autoComplete="off"
-                  maxLength={"255"}
-                  required
-                />
-                <Select
-                  className="place-self-stretch"
-                  id={"fk_tipo_valor"}
-                  label={"Modificar valor"}
-                  name={"fk_tipo_valor"}
-                  options={[{ label: "", value: "" }, ...tiposValores]}
-                  required
-                />
-                <Input
-                  id={"referencia_1"}
-                  label={"Referencia 1"}
-                  name={"referencia_1"}
-                  type="text"
-                  autoComplete="off"
-                  maxLength={"255"}
-                  required
-                />
-                <Input
-                  id={"referencia_2"}
-                  label={"Referencia 2"}
-                  name={"referencia_2"}
-                  type="text"
-                  autoComplete="off"
-                  maxLength={"255"}
-                />
-                <Input
-                  id={"referencia_3"}
-                  label={"Referencia 3"}
-                  name={"referencia_3"}
-                  type="text"
-                  autoComplete="off"
-                  maxLength={"255"}
-                />
-                <Input
-                  id={"referencia_4"}
-                  label={"Referencia 4"}
-                  name={"referencia_4"}
-                  type="text"
-                  autoComplete="off"
-                  maxLength={"255"}
-                />
-                <Input
-                  id={"referencia_5"}
-                  label={"Referencia 5"}
-                  name={"referencia_5"}
-                  type="text"
-                  autoComplete="off"
-                  maxLength={"255"}
-                />
-                <ButtonBar>
-                  <Button type={"submit"} disabled={loading}>
-                    Crear convenio pin
-                  </Button>
-                </ButtonBar>
-              </Form>
-            </Fragment>
-          )
-        ) : (
-          ""
+          </Fragment>
         )}
       </Modal>
     </Fragment>
