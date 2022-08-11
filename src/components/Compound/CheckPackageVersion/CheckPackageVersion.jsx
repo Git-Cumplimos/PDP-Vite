410888be4e3273bba62d26070af6fb63d908877d
Alvaro Casas Quintero <alvaro.casas@cumplimos.co>
2022-08-11 12:04:53 -0500

    Quitar validacion de cache

--- src/components/Compound/CheckPackageVersion/CheckPackageVersion.jsx
+++ src/components/Compound/CheckPackageVersion/CheckPackageVersion.jsx
@@ -1,26 +1,30 @@
 import { useEffect } from "react";
 import packageJson from "../../../../package.json";
 
-const CheckPackageVersion = () => {
-  const caching = () => {
-    let version = localStorage.getItem("version");
-    if (version != packageJson.version) {
-      if ("caches" in window) {
-        caches.keys().then((names) => {
-          names.forEach((name) => {
-            caches.delete(name);
-          });
-        });
-        window.location.reload(true);
-      }
-      localStorage.clear();
-      localStorage.setItem("version", packageJson.version);
-    }
-  };
+const urlprueba = `${process.env.REACT_APP_PRUEBA}`;
 
-  useEffect(() => {
-    caching();
-  }, []);
+const CheckPackageVersion = () => {
+  //   const caching = () => {
+  //     let version = localStorage.getItem("version");
+  //     console.log("prueba1", urlprueba);
+  //     if (version != packageJson.version) {
+  //       console.log("prueba2", urlprueba);
+  //       if ("caches" in window) {
+  //         caches.keys().then((names) => {
+  //           names.forEach((name) => {
+  //             caches.delete(name);
+  //           });
+  //         });
+  //         window.location.reload(true);
+  //       }
+  //       localStorage.clear();
+  //       localStorage.setItem("version", packageJson.version);
+  //     }
+  //   };
+
+  //   useEffect(() => {
+  //     caching();
+  //   }, []);
   return <></>;
 };
 
