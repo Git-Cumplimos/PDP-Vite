9f728a9b30a2e398644f3ee54009659320091f9e
Alvaro Casas Quintero <alvaro.casas@cumplimos.co>
2022-08-11 10:00:11 -0500

    Cambios recaudo

--- src/apps/Corresponsalia/CorresponsaliaDavivienda/Views/RecaudoServiciosPublicosPrivados/RecaudoServiciosPublicosPrivados.jsx
+++ src/apps/Corresponsalia/CorresponsaliaDavivienda/Views/RecaudoServiciosPublicosPrivados/RecaudoServiciosPublicosPrivados.jsx
@@ -13,12 +13,13 @@ import ButtonBar from "../../../../../components/Base/ButtonBar";
 import Form from "../../../../../components/Base/Form";
 import Input from "../../../../../components/Base/Input";
 import Modal from "../../../../../components/Base/Modal";
-import MoneyInput from "../../../../../components/Base/MoneyInput";
+import MoneyInput, {
+  formatMoney,
+} from "../../../../../components/Base/MoneyInput";
 import SimpleLoading from "../../../../../components/Base/SimpleLoading";
-import TableEnterprise from "../../../../../components/Base/TableEnterprise";
 import { useAuth } from "../../../../../hooks/AuthHooks";
-import useQuery from "../../../../../hooks/useQuery";
 import { notify, notifyError } from "../../../../../utils/notify";
+import TicketsDavivienda from "../../components/TicketsDavivienda";
 import {
   postConsultaConveniosDavivienda,
   postConsultaTablaConveniosEspecifico,
@@ -43,9 +44,35 @@ const RecaudoServiciosPublicosPrivados = () => {
     ref2: "",
     valor: "",
   });
+  const [objTicketActual, setObjTicketActual] = useState({
+    title: "Recibo de Pago de Recaudo de Facturas",
+    timeInfo: {
+      "Fecha de venta": "",
+      Hora: "",
+    },
+    commerceInfo: [
+      /*id transaccion recarga*/
+      /*id_comercio*/
+      ["Id comercio", roleInfo?.id_comercio ? roleInfo?.id_comercio : 0],
+      /*id_dispositivo*/
+      ["No. terminal", roleInfo?.id_dispositivo ? roleInfo?.id_dispositivo : 0],
+      /*ciudad*/
+      ["Municipio", roleInfo?.ciudad ? roleInfo?.ciudad : "Sin datos"],
+      /*direccion*/
+      ["Dirección", roleInfo?.direccion ? roleInfo?.direccion : "Sin datos"],
+      ["Tipo de operación", "Recaudo de facturas"],
+      ["", ""],
+    ],
+    commerceName: roleInfo?.["nombre comercio"]
+      ? roleInfo?.["nombre comercio"]
+      : "Sin datos",
+    trxInfo: [],
+    disclamer: "Línea de atención personalizada: #688\nMensaje de texto: 85888",
+  });
+  const [datosConsulta, setDatosConsulta] = useState({});
   const [isUploading, setIsUploading] = useState(true);
   const [convenio, setConvenio] = useState([]);
-  const [dataConveniosPagar, setDataConveniosPagar] = useState([3]);
+  const dataConveniosPagar = ["3", "0"];
   useEffect(() => {
     if (state?.id) {
       fecthTablaConveniosEspecificoFunc();
@@ -88,6 +115,29 @@ const RecaudoServiciosPublicosPrivados = () => {
       if (datosTrans.valor !== datosTransValidacion.valor) {
         return notifyError("El valor ingresado es diferente");
       }
+      const hoy = new Date();
+      const fecha =
+        hoy.getDate() + "-" + (hoy.getMonth() + 1) + "-" + hoy.getFullYear();
+      /*hora actual */
+      const hora =
+        hoy.getHours() + ":" + hoy.getMinutes() + ":" + hoy.getSeconds();
+      const objTicket = { ...objTicketActual };
+      objTicket["timeInfo"]["Fecha de venta"] = fecha;
+      objTicket["timeInfo"]["Hora"] = hora;
+      objTicket["trxInfo"].push(["Convenio", convenio.nom_convenio_cnb]);
+      objTicket["trxInfo"].push(["", ""]);
+      objTicket["trxInfo"].push(["Código convenio", convenio.cod_convenio_cnb]);
+      objTicket["trxInfo"].push(["", ""]);
+      objTicket["trxInfo"].push([
+        "Referencia 1",
+        datosTransValidacion?.ref1 ?? "",
+      ]);
+      objTicket["trxInfo"].push(["", ""]);
+      objTicket["trxInfo"].push([
+        "Referencia 2",
+        datosTransValidacion?.ref2 ?? "",
+      ]);
+      objTicket["trxInfo"].push(["", ""]);
       setIsUploading(true);
       postRecaudoConveniosDavivienda({
         valTipoConsultaConvenio: "2",
@@ -100,6 +150,8 @@ const RecaudoServiciosPublicosPrivados = () => {
         valor: datosTransValidacion?.valor ?? "0",
         valReferencia1: datosTransValidacion?.ref1 ?? "",
         valReferencia2: datosTransValidacion?.ref2 ?? "",
+        nomConvenio: convenio.nom_convenio_cnb,
+        ticket: objTicket,
 
         idComercio: roleInfo?.id_comercio,
         idUsuario: roleInfo?.id_usuario,
@@ -126,7 +178,6 @@ const RecaudoServiciosPublicosPrivados = () => {
           notifyError("No se ha podido conectar al servidor");
           console.error(err);
         });
-      console.log("realizar pago");
     } else {
       setIsUploading(true);
       postConsultaConveniosDavivienda({
@@ -148,6 +199,9 @@ const RecaudoServiciosPublicosPrivados = () => {
           if (res?.status) {
             setIsUploading(false);
             notify(res?.msg);
+            console.log("SSSS", res?.obj);
+            setDatosConsulta(res?.obj);
+            setShowModal((old) => ({ ...old, estadoPeticion: 1 }));
           } else {
             setIsUploading(false);
             notifyError(res?.msg);
@@ -168,6 +222,30 @@ const RecaudoServiciosPublicosPrivados = () => {
       ref2: "",
       valor: "",
     }));
+    setObjTicketActual((old) => {
+      return {
+        ...old,
+        commerceInfo: [
+          /*id transaccion recarga*/
+          /*id_comercio*/
+          ["Id comercio", roleInfo?.id_comercio ? roleInfo?.id_comercio : ""],
+          /*id_dispositivo*/
+          [
+            "No. terminal",
+            roleInfo?.id_dispositivo ? roleInfo?.id_dispositivo : "",
+          ],
+          /*ciudad*/
+          ["Municipio", roleInfo?.ciudad ? roleInfo?.ciudad : ""],
+          /*direccion*/
+          ["Dirección", roleInfo?.direccion ? roleInfo?.direccion : ""],
+          ["Tipo de operación", "Recaudo de facturas"],
+          ["", ""],
+        ],
+        trxInfo: [],
+      };
+    });
+    setDatosConsulta({});
+
     setShowModal((old) => ({ ShowModal: false, estadoPeticion: 0 }));
   }, []);
   const onChangeMoney = (ev, valor) => {
@@ -196,7 +274,7 @@ const RecaudoServiciosPublicosPrivados = () => {
               label={convenio?.nom_ref1_cnb}
               type='text'
               name='ref1'
-              minLength='4'
+              minLength='1'
               maxLength='32'
               required
               value={datosTrans.ref1}
@@ -215,7 +293,7 @@ const RecaudoServiciosPublicosPrivados = () => {
             label={convenio?.nom_ref2_cnb}
             type='text'
             name='ref2'
-            minLength='4'
+            minLength='1'
             maxLength='32'
             required
             value={datosTrans.ref2}
@@ -250,29 +328,27 @@ const RecaudoServiciosPublicosPrivados = () => {
       <Modal show={showModal} handleClose={handleClose}>
         {estadoPeticion === 0 ? (
           <>
-            <h1 className='text-2xl text-center mb-10'>
+            <h1 className='text-2xl text-center mb-10 font-semibold'>
               Ingrese nuevamente los datos de la transacción
             </h1>
             <Form grid onSubmit={onSubmitValidacion}>
               {convenio?.ctrol_ref1_cnb === "1" && (
-                <>
-                  <Input
-                    id='ref1'
-                    label={convenio?.nom_ref1_cnb}
-                    type='text'
-                    name='ref1'
-                    minLength='4'
-                    maxLength='32'
-                    required
-                    value={datosTransValidacion.ref1}
-                    onInput={(e) => {
-                      let valor = e.target.value;
-                      let num = valor.replace(/[\s\.]/g, "");
-                      setDatosTransValidacion((old) => {
-                        return { ...old, ref1: num };
-                      });
-                    }}></Input>
-                </>
+                <Input
+                  id='ref1'
+                  label={convenio?.nom_ref1_cnb}
+                  type='text'
+                  name='ref1'
+                  minLength='1'
+                  maxLength='32'
+                  required
+                  value={datosTransValidacion.ref1}
+                  onInput={(e) => {
+                    let valor = e.target.value;
+                    let num = valor.replace(/[\s\.]/g, "");
+                    setDatosTransValidacion((old) => {
+                      return { ...old, ref1: num };
+                    });
+                  }}></Input>
               )}
               {convenio?.ctrol_ref2_cnb === "1" && (
                 <Input
@@ -280,7 +356,7 @@ const RecaudoServiciosPublicosPrivados = () => {
                   label={convenio?.nom_ref2_cnb}
                   type='text'
                   name='ref2'
-                  minLength='4'
+                  minLength='1'
                   maxLength='32'
                   required
                   value={datosTransValidacion.ref2}
@@ -292,7 +368,6 @@ const RecaudoServiciosPublicosPrivados = () => {
                     });
                   }}></Input>
               )}
-              {console.log(dataConveniosPagar)}
               {dataConveniosPagar.includes(convenio?.num_ind_consulta_cnb) && (
                 <MoneyInput
                   id='valCashOut'
@@ -323,6 +398,62 @@ const RecaudoServiciosPublicosPrivados = () => {
                 </Button>
               </ButtonBar>
             </Form>
+          </>
+        ) : estadoPeticion === 1 ? (
+          <>
+            <h1 className='text-2xl text-center mb-10 font-semibold'>
+              Resultado consulta
+            </h1>
+            {/* <h2 className='text-base'>
+                {`Valor de transacción: ${formatMoney.format(
+                  // datosTrans.valorCashOut
+                )} `}
+              </h2> */}
+            {/* <h2>{`Número Daviplata: ${datosTrans.numeroTelefono}`}</h2> */}
+            {/* <h2>{`Número de otp: ${datosTrans.otp}`}</h2> */}
+            <ButtonBar>
+              <Button onClick={handleClose}>Cancelar</Button>
+              <Button type='submit' onClick={() => {}}>
+                Aceptar
+              </Button>
+            </ButtonBar>
+          </>
+        ) : estadoPeticion === 2 ? (
+          <>
+            <h1 className='text-2xl text-center mb-10 font-semibold'>
+              ¿Esta seguro de realizar el pago del servicio?
+            </h1>
+            {/* <h2 className='text-base'>
+                {`Valor de transacción: ${formatMoney.format(
+                  // datosTrans.valorCashOut
+                )} `}
+              </h2> */}
+            {/* <h2>{`Número Daviplata: ${datosTrans.numeroTelefono}`}</h2> */}
+            {/* <h2>{`Número de otp: ${datosTrans.otp}`}</h2> */}
+            <ButtonBar>
+              <Button onClick={handleClose}>Cancelar</Button>
+              <Button type='submit' onClick={() => {}}>
+                Aceptar
+              </Button>
+            </ButtonBar>
+          </>
+        ) : estadoPeticion === 3 ? (
+          <>
+            <h2>
+              <ButtonBar>
+                <Button onClick={handlePrint}>Imprimir</Button>
+                <Button
+                  type='submit'
+                  onClick={() => {
+                    handleClose();
+                  }}>
+                  Aceptar
+                </Button>
+              </ButtonBar>
+            </h2>
+            <TicketsDavivienda
+              ticket={objTicketActual}
+              refPrint={printDiv}></TicketsDavivienda>
           </>
         ) : (
           <></>
