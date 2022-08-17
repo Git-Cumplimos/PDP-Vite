1502f6e8eb52f321480c2338281ba0ba5a1c8c29
Hector Melgarejo <hector.agudelo@cumplimos.co>
2022-08-17 15:31:22 -0500

    Firma digital pinesVUS

--- src/apps/Corresponsalia/CorresponsaliaDavivienda/Views/Daviplata/Deposito.jsx
+++ src/apps/Corresponsalia/CorresponsaliaDavivienda/Views/Daviplata/Deposito.jsx
@@ -322,7 +322,7 @@ const Deposito = () => {
             maxLength={"10"}
             value={phone}
             onInput={(e) => {
-              if ((phone?.length === 0) & (e.target.value !== "3")) {
+              if ((String(phone).length > 0 & String(e.target.value).slice(0,1) !== "3")) {
                 notifyError("El número de celular debe iniciar por 3");
                 setPhone("");
               } else {
@@ -342,7 +342,7 @@ const Deposito = () => {
             maxLength={"10"}
             value={verificacionTel}
             onInput={(e) => {
-              if ((verificacionTel?.length === 0) & (e.target.value !== "3")) {
+              if ((String(phone).length > 0 & String(e.target.value).slice(0,1) !== "3")) {
                 notifyError("El número de celular debe iniciar por 3");
                 setVerificacionTel("");
               } else {
