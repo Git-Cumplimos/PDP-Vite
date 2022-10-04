3653c8c9ff2964890dac02f4d0774f79c2862e1d
Alvaro Casas Quintero <alvaro.casas@cumplimos.co>
2022-10-04 12:09:52 -0500

    Fix: reintento recaudo

--- src/apps/Corresponsalia/CorresponsaliaDavivienda/utils/fetchRecaudoServiciosPublicosPrivados.js
+++ src/apps/Corresponsalia/CorresponsaliaDavivienda/utils/fetchRecaudoServiciosPublicosPrivados.js
@@ -92,18 +92,67 @@ export const postRecaudoConveniosDavivienda = async (bodyObj) => {
   }
   try {
     const res = await fetchData(
-      `${urlDaviplata}davivienda_recaudo_servicios_publicos_privados/recaudo_servicios_publicos_privados`,
+      `${urlDaviplata}davivienda_recaudo_servicios_publicos_privados/prueba`,
       "POST",
       {},
       bodyObj,
       {},
-      true
+      true,
+      30000
     );
     if (!res?.status) {
-      console.error(res?.msg);
+      console.error(res?.msg ?? res?.message ?? "");
     }
     return res;
   } catch (err) {
+    console.log("error api gateway");
     throw err;
   }
 };
+
+export const postCheckReintentoRecaudoConveniosDavivienda = async (bodyObj) => {
+  if (!bodyObj) {
+    return "Sin datos body";
+  }
+  try {
+    const res = await fetchData(
+      `${urlDaviplata}davivienda_recaudo_servicios_publicos_privados/check_reintento_recaudo_servicios_publicos_privados`,
+      "POST",
+      {},
+      bodyObj,
+      {},
+      true,
+      30000
+    );
+    if (!res?.status) {
+      console.error(res?.msg ?? res?.message ?? "");
+    }
+    return res;
+  } catch (err) {
+    console.log("error api gateway");
+    throw err;
+  }
+};
+
+// export const postRecaudoConveniosDavivienda = async (bodyObj) => {
+//   if (!bodyObj) {
+//     return "Sin datos body";
+//   }
+//   try {
+//     const res = await fetchData(
+//       `${urlDaviplata}davivienda_recaudo_servicios_publicos_privados/recaudo_servicios_publicos_privados`,
+//       "POST",
+//       {},
+//       bodyObj,
+//       {},
+//       true,
+//       30000
+//     );
+//     if (!res?.status) {
+//       console.error(res?.msg);
+//     }
+//     return res;
+//   } catch (err) {
+//     throw err;
+//   }
+// };
