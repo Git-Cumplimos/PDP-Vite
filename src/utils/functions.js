b56124bdc316e33375aeaddecab1b2525dacc54e
Nicholas David <nicholas.carreno@cumplimos.co>
2022-10-07 13:47:42 -0500

    test phone number format

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -55,6 +55,17 @@ export const onChangeNumber = (ev) => {
 export const toAccountNumber = (num = "") =>
   num.replace(/(.{4})/g, "$1 ").trim();
 
+export const toPhoneNumber = (num = "") => {
+  let reg = /(\d{1,3}[-.\s]?)?(\d{1,3}[-.\s]?)?(\d{1,4})/;
+  return (
+    num
+      .match(reg)
+      ?.filter((val, ind) => ind > 0 && val !== undefined)
+      .map((val) => val.trim().replace("-", ""))
+      .join(" ") ?? num
+  );
+};
+
 /**
  * On change for account number inputs
  * @param {*} ev
