2c6714566a2061f2f053b2083a52a62b22fc3177
Nicholas David <nicholas.carreno@cumplimos.co>
2022-10-31 15:14:53 -0500

    fix(cierre): agregar permiso para ver reporte de trx

---
+++ src/pages/Gestion/Views/ArqueoCierre/permissionsEnum.ts
@@ -0,0 +1,11 @@
+export enum PermissionsCaja {
+  VerReporteTrxCierre = 6108,
+  VerHistoricoCierresCaja = 6107,
+  VerHistoricoNotasDebitoCredito = 6106,
+  AgregarNotasCredito = 6105,
+  AgregarNotasDebito = 6104,
+  AgregarEntidades = 6103,
+  AnalizarComprobantes = 6102,
+  AgregaComprobantes = 6101,
+  RealizarArqueoCierre = 6100,
+}
