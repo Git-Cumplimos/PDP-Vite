f0d799f790c1f475adbe6dda49f3b31fe5b44cea
Alvaro Casas Quintero <alvaro.casas@cumplimos.co>
2022-11-01 11:40:12 -0500

    Fix: quitar console.log de crypto utils

--- src/utils/cryptoUtils.js
+++ src/utils/cryptoUtils.js
@@ -3,7 +3,6 @@ import CryptoJS from "crypto-js";
 export const cifrarAES = (llave, iv, texto) => {
   const derived_key = CryptoJS.enc.Base64.parse(llave);
   iv = CryptoJS.enc.Utf8.parse(iv);
-  // console.log(iv);
   return CryptoJS.AES.encrypt(texto, derived_key, {
     iv: iv,
   }).toString();
