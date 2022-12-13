1ca45c7d08d000fa593bf6a55658d45a1803a912
Nicholas David <nicholas.carreno@cumplimos.co>
2022-12-13 11:36:34 -0500

    feat: change carousel ratio and relative size
    (cherry picked from commit 0b70c593196733cc0a8dfb0f679c874b19046ebc)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,13 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useMemo } from "react";
 import { useImgs } from "../hooks/ImgsHooks";
+import { useWindowSize } from "../hooks/WindowSizeHooks";
 // import ReconoSERID from "../components/Compound/ReconoSERID/ReconoSERID";
 
 const Home = () => {
   const { urlsPrivateApps } = useUrls();
   const { banners } = useImgs();
+  const [ width ] = useWindowSize()
 
   // const [emails, setEmails] = useState([
   //   "directora.mercadeo@puntodepago.com.co",
@@ -30,10 +32,12 @@ const Home = () => {
         showArrows={false}
         showThumbs={false}
         showStatus={false}
+        showIndicators={width > 1024}
+        className="w-3/4 mx-auto hidden md:block"
       >
         {imgsCarousel.map(({ name, url }) => {
           return (
-            <div className="aspect-w-16 aspect-h-5" key={url}>
+            <div className="aspect-w-16 aspect-h-2" key={url}>
               <img alt={name} src={url} className="object-cover" />
             </div>
           );
