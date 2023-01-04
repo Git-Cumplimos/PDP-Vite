09df5c1b5112b724d3334dee299e9f32ff52395e
Alvaro Casas Quintero <alvaro.casas@cumplimos.co>
2023-01-04 17:31:05 -0500

    Feat: reporte conteo comision

--- src/pages/Reportes/routes.jsx
+++ src/pages/Reportes/routes.jsx
@@ -3,18 +3,27 @@ import { lazy } from "react";
 
 const Koncilia = lazy(() => import("./views/Koncilia"));
 const ReporteComisiones = lazy(() => import("./views/ReporteComisiones"));
+const ReporteConteoComisiones = lazy(() =>
+  import("./views/ReporteConteoComisiones")
+);
 
 export const rutasReportes = [
   {
     link: "/reportes/koncilia",
-    label: <AppIcons Logo={"RECAUDO"} name="Reportes koncilia" />,
+    label: <AppIcons Logo={"RECAUDO"} name='Reportes koncilia' />,
     component: Koncilia,
     permission: [40],
   },
   {
     link: "/reportes/comisiones",
-    label: <AppIcons Logo={"RECAUDO"} name="Reportes comisiones" />,
+    label: <AppIcons Logo={"RECAUDO"} name='Reportes comisiones' />,
     component: ReporteComisiones,
+    permission: [42],
+  },
+  {
+    link: "/reportes/conteo-comisiones",
+    label: <AppIcons Logo={"RECAUDO"} name='Reportes conteo comisiones' />,
+    component: ReporteConteoComisiones,
     permission: [42],
   },
 ];
--- src/pages/Reportes/utils/fetchReportesComisiones.js
+++ src/pages/Reportes/utils/fetchReportesComisiones.js
@@ -22,3 +22,44 @@ export const postObtenerReporteComisionesAplicadas = async (bodyObj) => {
     throw err;
   }
 };
+
+export const postObtenerReporteConteoComisionesAplicadas = async (bodyObj) => {
+  if (!bodyObj) {
+    return "Sin datos body";
+  }
+  try {
+    const res = await fetchData(
+      `${urlComisiones}/servicio-reporte-aplicacion-comision/reporte-conteo-aplicacion-comision`,
+      "POST",
+      {},
+      bodyObj
+    );
+    if (!res?.status) {
+      console.error(res?.msg);
+    }
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
+export const postObtenerReporteHistoricoConteoComisionesAplicadas = async (
+  bodyObj
+) => {
+  if (!bodyObj) {
+    return "Sin datos body";
+  }
+  try {
+    const res = await fetchData(
+      `${urlComisiones}/servicio-reporte-aplicacion-comision/reporte-historico-conteo-aplicacion-comision`,
+      "POST",
+      {},
+      bodyObj
+    );
+    if (!res?.status) {
+      console.error(res?.msg);
+    }
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
