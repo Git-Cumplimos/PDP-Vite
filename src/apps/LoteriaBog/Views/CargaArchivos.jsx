69b320b764effe49aed965f5436662de935de0b5
LisbethNunez13 <lisbeth.nunez@cumplimos.co>
2023-02-08 16:33:40 -0500

    Console.log para validar carga de archivos loterías

--- src/apps/LoteriaBog/Views/CargaArchivos.jsx
+++ src/apps/LoteriaBog/Views/CargaArchivos.jsx
@@ -80,6 +80,7 @@ const CargaArchivos = ({ route }) => {
       };
       fetchData(url_cargueS3, "GET", query)
         .then((respuesta) => {
+          console.log("Por aca****", respuesta?.msg)
           if (!respuesta?.status) {
             notifyError(respuesta?.msg === "Motivo: Archivo con errores: UniqueViolation" ? "Este archivo ya fue cargado previamente" : respuesta?.msg);
             console.log("Si entro =?=?=?=?=?=?", respuesta?.msg)
