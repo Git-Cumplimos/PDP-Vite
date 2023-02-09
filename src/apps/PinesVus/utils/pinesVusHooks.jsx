cd7c0fa9d419026a2b6acf3d02a12495e0a239e5
Hector Melgarejo <hector.agudelo@cumplimos.co>
2023-02-09 12:36:17 -0500

    Cambios en pines vus y transacciones sobre tickets

--- src/apps/PinesVus/utils/pinesVusHooks.jsx
+++ src/apps/PinesVus/utils/pinesVusHooks.jsx
@@ -49,7 +49,7 @@ export const useProvidePinesVus = () => {
   const { roleInfo, pdpUser} = useAuth();
   const [activarNavigate, setActivarNavigate] = useState(true);
   console.log("PDP USER",pdpUser)
-  const cancelPinVus = useCallback(async (valor, motivo, trx, user, id_pin, valor_tramite, tipCancelacion, infoComercioCreacion) => {
+  const cancelPinVus = useCallback(async (valor, motivo, trx, user, id_pin, valor_tramite, tipCancelacion, infoComercioCreacion,ticket) => {
     let tipo_comercio = user?.tipo_comercio
     if (user?.tipo_comercio === "KIOSCO"){
       tipo_comercio = "OFICINAS PROPIAS"
@@ -66,7 +66,8 @@ export const useProvidePinesVus = () => {
       motivo: motivo,
       tipCancelacion: tipCancelacion,
       id_trx: trx,
-      comercio_creacion: infoComercioCreacion
+      comercio_creacion: infoComercioCreacion,
+      ticket: ticket
     };
     const query = {
       id_pin: id_pin,
@@ -117,7 +118,7 @@ export const useProvidePinesVus = () => {
   }, [roleInfo, pdpUser]);
 
   const usarPinVus = useCallback(
-    async (valor, trx, num_tramite, user, id_pin) => {
+    async (valor, trx, num_tramite, user, id_pin, ticket) => {
       let tipo_comercio = user?.tipo_comercio
       if (user?.tipo_comercio === "KIOSCO"){
         tipo_comercio = "OFICINAS PROPIAS"
@@ -131,6 +132,7 @@ export const useProvidePinesVus = () => {
         NombreComercio: roleInfo?.["nombre comercio"],
         valor: parseFloat(valor),
         id_trx: trx,
+        ticket: ticket
       };
       if (num_tramite !== "") {
         body.num_tramite = String(num_tramite);
