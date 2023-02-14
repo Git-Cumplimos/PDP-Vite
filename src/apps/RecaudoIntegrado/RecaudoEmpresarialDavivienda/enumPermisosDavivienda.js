f54d3c335ca94da56fe42dddeb82bdc7c4bff732
cristhiancao9 <u1802342@unimilitar.edu.co>
2023-02-14 10:21:13 -0500

    enum davivienda

--- src/apps/RecaudoIntegrado/RecaudoEmpresarialDavivienda/enumPermisosDavivienda.js
+++ src/apps/RecaudoIntegrado/RecaudoEmpresarialDavivienda/enumPermisosDavivienda.js
@@ -1,10 +1,4 @@
 export const enumPermisosDavivienda = Object.freeze({
-  davivienda_cb_cash_in: 5001,
-  davivienda_cb_cash_out: 5002,
-  davivienda_cb_pago_por_giro: 5003,
-  davivienda_cb_depositos: 5004,
-  davivienda_cb_retiros: 5005,
-  davivienda_cb_recaudo: 5006,
-  davivienda_cb_pago_productos_propios: 5007,
-  davivienda_cb_recaudo_operaciones: 5008,
+  Recaudo_empresarial_PDP: 4001,
+  Recaudo_empresarial_contingencia_PDP: 4002,
 });
