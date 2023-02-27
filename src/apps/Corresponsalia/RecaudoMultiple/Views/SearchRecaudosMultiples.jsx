65838d16a2bed8ef9f4fee2f1b0c5cb6bbfcfbde
Alvaro Casas Quintero <alvaro.casas@cumplimos.co>
2023-02-27 08:31:08 -0500

    Feat: unificar consulta paginada de recaudos multiples con consutla individual

--- src/apps/Corresponsalia/RecaudoMultiple/Views/SearchRecaudosMultiples.jsx
+++ src/apps/Corresponsalia/RecaudoMultiple/Views/SearchRecaudosMultiples.jsx
@@ -2,11 +2,17 @@ import { Fragment, useCallback, useEffect, useMemo, useState } from "react";
 import TableEnterprise from "../../../../components/Base/TableEnterprise";
 import Input from "../../../../components/Base/Input";
 import { fetchRecaudosMultiples } from "../utils/fetchRecaudoMultiple";
+import ConsultarRecaudosMultiples from "../components/ConsultarRecaudosMultiples";
+import { useAuth } from "../../../../hooks/AuthHooks";
+import ButtonBar from "../../../../components/Base/ButtonBar";
+import Button from "../../../../components/Base/Button";
 
 const SearchRecaudosMultiples = () => {
   const [dataRecaudoMultiple, setDataRecaudoMultiple] = useState({
     pk_tbl_recaudo_multiple: "",
   });
+  const [uuid, setUuid] = useState("");
+  const { roleInfo, pdpUser } = useAuth();
   const [recaudosMultiples, setRecaudosMultiples] = useState([]);
   const [maxPages, setMaxPages] = useState(0);
   const [{ page, limit }, setPageData] = useState({
@@ -65,46 +71,62 @@ const SearchRecaudosMultiples = () => {
   }, [page, limit, dataRecaudoMultiple]);
   const onClickRow = useCallback(
     (ev, i) => {
-      // setSelectedGruposComercios((old) => ({
-      //   ...old,
-      //   nombre_grupo_plan: gruposPlanes[i]?.nombre_grupo_plan,
-      //   fk_tbl_grupo_planes_comisiones:
-      //     gruposPlanes[i]?.pk_tbl_grupo_planes_comisiones,
-      // }));
-      // handleClose();
+      setUuid(recaudosMultiples[i]?.pk_tbl_recaudo_multiple);
     },
-    [tableRecaudosMultiples]
+    [recaudosMultiples]
   );
+  const resetUuid = (e) => {
+    e.preventDefault();
+    setUuid("");
+  };
   return (
     <>
-      <TableEnterprise
-        title='Recaudos multiples'
-        maxPage={maxPages}
-        headers={[
-          "Id",
-          "Total transacciones",
-          "Transacciones finalizadas",
-          "Fecha",
-        ]}
-        data={tableRecaudosMultiples}
-        onSelectRow={onClickRow}
-        onSetPageData={setPageData}>
-        <Input
-          id='pk_tbl_recaudo_multiple'
-          label='Id recaudo multiple'
-          type='text'
-          name='pk_tbl_recaudo_multiple'
-          minLength='1'
-          maxLength='36'
-          autoComplete='false'
-          // required
-          value={dataRecaudoMultiple.pk_tbl_recaudo_multiple}
-          onInput={(e) => {
-            setDataRecaudoMultiple((old) => {
-              return { ...old, pk_tbl_recaudo_multiple: e.target.value };
-            });
-          }}></Input>
-      </TableEnterprise>
+      {uuid === "" ? (
+        <TableEnterprise
+          title='Recaudos multiples'
+          maxPage={maxPages}
+          headers={[
+            "Id",
+            "Total transacciones",
+            "Transacciones finalizadas",
+            "Fecha creación",
+          ]}
+          data={tableRecaudosMultiples}
+          onSelectRow={onClickRow}
+          onSetPageData={setPageData}>
+          <Input
+            id='pk_tbl_recaudo_multiple'
+            label='Id recaudo multiple'
+            type='text'
+            name='pk_tbl_recaudo_multiple'
+            minLength='1'
+            maxLength='36'
+            autoComplete='false'
+            // required
+            value={dataRecaudoMultiple.pk_tbl_recaudo_multiple}
+            onInput={(e) => {
+              setDataRecaudoMultiple((old) => {
+                return { ...old, pk_tbl_recaudo_multiple: e.target.value };
+              });
+            }}></Input>
+        </TableEnterprise>
+      ) : (
+        <div className='grid grid-flow-row auto-rows-max gap-4 place-items-center text-center'>
+          <h1 className='text-3xl text-center mb-10 mt-5'>
+            Consulta recaudo multiple
+          </h1>
+          <ConsultarRecaudosMultiples
+            uuid={uuid}
+            roleInfo={roleInfo}
+            pdpUser={pdpUser}
+          />
+          <ButtonBar>
+            <Button type='submit' onClick={resetUuid}>
+              Volver a ingresar indentificador
+            </Button>
+          </ButtonBar>
+        </div>
+      )}
     </>
   );
 };
