5846d09f82251ebb99a91c59dd6431ac9d46f31b
KevinG3511 <kevin.guevara@cumplimos.co>
2023-02-27 09:52:54 -0500

    feat: tabla de retiro conectado a la bbdd

--- src/apps/RecaudoDirecto/Views/Retiro/FormularioRetiro.jsx
+++ src/apps/RecaudoDirecto/Views/Retiro/FormularioRetiro.jsx
@@ -1,44 +1,63 @@
-import { Fragment, useCallback, useEffect, useState } from "react";
-import { useNavigate, Navigate, useParams, useSearchParams } from "react-router-dom";
+import { Fragment, useCallback, useState } from "react";
+import { useParams } from "react-router-dom";
 import Button from "../../../../components/Base/Button";
 import ButtonBar from "../../../../components/Base/ButtonBar";
 import Form from "../../../../components/Base/Form";
+import Modal from "../../../../components/Base/Modal";
 import Input from "../../../../components/Base/Input";
 
 const retirosDirectos = [
-  { pk_id_convenio: 2041, otp: 650122, nombre_cliente: "Kevin Guevara", monto: "25000" },
-  { pk_id_convenio: 2041, otp: 660122, nombre_cliente: "Juliana Reyes", monto: "35000" },
-  { pk_id_convenio: 2042, otp: 670122, nombre_cliente: "Camila Hernandez", monto: "5.000" },
-  { pk_id_convenio: 2043, otp: 680122, nombre_cliente: "Nicolas Guerrero", monto: "125.000" },
+  { pk_id_convenio: 1, otp: 650122, nombre_cliente: "Kevin Guevara", monto: "25000" },
+  { pk_id_convenio: 3, otp: 660122, nombre_cliente: "Juliana Reyes", monto: "35000" },
+  { pk_id_convenio: 4, otp: 670122, nombre_cliente: "Camila Hernandez", monto: "5000" },
+  { pk_id_convenio: 6, otp: 680122, nombre_cliente: "Nicolas Guerrero", monto: "125000" },
+  { pk_id_convenio: 7, otp: 680122, nombre_cliente: "Juan Guerrero", monto: "125000" },
+  { pk_id_convenio: 8, otp: 680122, nombre_cliente: "Carlos Gonzales", monto: "125000" },
 ]
 const FormularioRetiro = () => {
+
   const { pk_id_convenio } = useParams();
   const { nombre_convenio } = useParams();
   // const [datos]=  useSearchParams();
-
-
-  const [id_convenio, setId_Convenio] = useState(null);
+  // const [id_convenio, setId_Convenio] = useState(null);
   const [otp, setOtp] = useState('');
-  const [tipoDocumento, setTipoDocumento] = useState(null);
+  // const [tipoDocumento, setTipoDocumento] = useState(null);
   const [documento, setDocumento] = useState(null);
   const [monto, setMonto] = useState(null);
+  const [dataRetiro, setDataRetiro] = useState('')
+  const [showModal, setShowModal] = useState(false)
 
-  const consultarRetiroDirecto = useCallback((e) => {
+  const consultarRetiroD = (e) => {
+    // Datos Estaticos
+    e.preventDefault()
+    const buscarRetiro = retirosDirectos.filter((data) => {
+      return parseInt(data.pk_id_convenio) === parseInt(pk_id_convenio) && parseInt(data.otp) === parseInt(otp)
+    })
+    if (buscarRetiro.length < 1) { setOtp(''); alert("Datos Incorrectos"); return false }
+    setDataRetiro(buscarRetiro)
+    setShowModal(true);
+  }
+
+  const hacerRetiro = useCallback((e) => {
     e.preventDefault();
     // Funcionalidad Basica ()
-    const buscarCuentaRetiro = retirosDirectos.filter((data) => {
-      return data.pk_id_convenio === e.target.Codigo_nit.value && parseInt(data.otp) === parseInt(otp)
-    })
+    const buscarCuentaRetiro = dataRetiro
     if (buscarCuentaRetiro.length < 1) { setOtp(''); setMonto(''); setDocumento(''); alert("Datos Incorrectos"); return false }
     if (parseInt(monto) <= parseInt(buscarCuentaRetiro[0].monto)) {
       alert("Retiro Exitoso")
       buscarCuentaRetiro[0].monto = buscarCuentaRetiro[0].monto - monto
+      setShowModal(false);
+      setOtp(''); setMonto(''); setDocumento('');
     } else { alert("Saldo insuficiente"); setMonto('') }
-  }, [otp, monto])
+  }, [dataRetiro, monto])
 
+  const handleClose = useCallback(() => {
+    setShowModal(false);
+  }, []);
   return (
-    <Fragment><h2 className="text-3xl mx-auto text-center mb-4"> Realizar retiro </h2>
-      <Form onSubmit={consultarRetiroDirecto} grid >
+    <Fragment>
+      <h2 className="text-3xl mx-auto text-center mb-4"> Realizar retiro </h2>
+      <Form onSubmit={consultarRetiroD} grid >
         <Input
           id={"Codigo_nit"}
           label={"Codigo convenio"}
@@ -80,23 +99,56 @@ const FormularioRetiro = () => {
           onChange={(e) => { setDocumento(e.target.value) }}
           autoComplete="off"
           required />
-        <Input
-          id={1}
-          label={"Monto"}
-          name={"monto"}
-          type="text"
-          minLength={"1"}
-          maxLength={"15"}
-          value={monto ?? ""}
-          onChange={(e) => { setMonto(e.target.value) }}
-          autoComplete="off"
-          required />
         <ButtonBar className={"lg:col-span-2"}>
           <Button type={"submit"} >
-            Retirar
+            Consultar
           </Button>
         </ButtonBar>
-      </Form></Fragment>
+      </Form>
+      {dataRetiro !== '' &&
+        <Modal show={showModal} handleClose={handleClose}>
+          <h2 className="text-3xl mx-auto text-center mb-4"> Realizar recaudo </h2>
+          <Form onSubmit={hacerRetiro} grid >
+            <Input
+              id={"Codigo_nit"}
+              label={"Codigo convenio"}
+              name={"Codigo_nit"}
+              autoComplete="off"
+              defaultValue={dataRetiro[0].pk_id_convenio}
+              disabled
+              required />
+            <Input
+              id={1}
+              label={"Nombre cliente"}
+              name={"nombre_cliente"}
+              type="text"
+              defaultValue={dataRetiro[0].nombre_cliente ?? ""}
+              autoComplete="off"
+              disabled
+              required />
+            <Input
+              id={1}
+              label={"Monto"}
+              name={"monto"}
+              type="text"
+              minLength={"1"}
+              maxLength={"15"}
+              value={monto ?? ""}
+              onChange={(e) => { setMonto(e.target.value) }}
+              autoComplete="off"
+              required />
+            <ButtonBar>
+              <Button type={"submit"} >
+                Aceptar
+              </Button>
+              <Button onClick={()=>handleClose()} >
+                Cancelar
+              </Button>
+            </ButtonBar>
+          </Form>
+        </Modal>
+      }
+    </Fragment>
   )
 }
 
--- src/apps/RecaudoDirecto/Views/Retiro/RetiroDirecto.jsx
+++ src/apps/RecaudoDirecto/Views/Retiro/RetiroDirecto.jsx
@@ -1,85 +1,31 @@
 import { Fragment, useCallback, useEffect, useState } from "react";
-import { useNavigate, Navigate, useParams, useSearchParams } from "react-router-dom";
-import Modal from "../../../../components/Base/Modal";
-import Button from "../../../../components/Base/Button";
-import ButtonBar from "../../../../components/Base/ButtonBar";
+import { useNavigate} from "react-router-dom";
 import TableEnterprise from "../../../../components/Base/TableEnterprise";
-import ToggleInput from "../../../../components/Base/ToggleInput";
-import Select from "../../../../components/Base/Select";
-import Form from "../../../../components/Base/Form";
 import Input from "../../../../components/Base/Input";
-
-// import getRecaudosList from "../utils/fetchFunctions"
-
-
-const convenioRetiro = {
-  "name": "State",
-  "value": [
-    { activo: true, codigo_ean_iac: '0000000000000', pk_id_convenio: 2041, nombre_convenio: 'pruebas', fecha_creacion: '2022-07-10', },
-    { activo: true, codigo_ean_iac: '8978945645614', pk_id_convenio: 2037, nombre_convenio: 'pruebas2', fecha_creacion: '2023-05-06', },
-  ],
-}
-const retirosDirectos = [
-  { pk_id_convenio: 2041, otp: 650122, nombre_cliente: "Kevin Guevara", monto: "25000" },
-  { pk_id_convenio: 2041, otp: 660122, nombre_cliente: "Juliana Reyes", monto: "35000" },
-  { pk_id_convenio: 2042, otp: 670122, nombre_cliente: "Camila Hernandez", monto: "5.000" },
-  { pk_id_convenio: 2043, otp: 680122, nombre_cliente: "Nicolas Guerrero", monto: "125.000" },
-]
+import { getRetirosList } from "../../utils/fetchFunctions"
 
 const RetiroDirecto = () => {
   const navigate = useNavigate()
-
-  const [showModal, setShowModal] = useState(false)
-  const [selected, setSelected] = useState('');
-  const [id_convenio, setId_Convenio] = useState(null);
-  const [otp, setOtp] = useState('');
-  const [tipoDocumento, setTipoDocumento] = useState(null);
-  const [documento, setDocumento] = useState(null);
-  const [monto, setMonto] = useState(null);
-
-  const optionsDocumento = [
-    { value: "01", label: "Cédula Ciudadanía" },
-    { value: "02", label: "Cédula Extranjería" },
-    { value: "04", label: "Tarjeta Identidad" },
-    { value: "13", label: "Registro Civil" },
-  ];
   const [busqueda, setBusqueda] = useState('');
-  // const [listRetiros,setListRetiros] = useState('')
 
-  // const getRecaudos = useCallback(() => {
-  //     getRecaudosList()
-  //     .then((res)=>{res.json()})
-  //     .then((data)=>{setListRetiros(data)})
-  // },[/*pages*/])
 
-  // useEffect(()=>{getRecaudos()},[])
+  const [listRetiro, setListRetiros] = useState('')
+  const [cargando, setCargando] = useState(false)
+  const [pageData, setPageData] = useState({ page: 1, limit: 10 });
+  const [maxPages, setMaxPages] = useState(0);
 
-  const consultarRetiroDirecto = useCallback((e) => {
-    e.preventDefault();
-    // Funcionalidad Basica ()
-    const buscarCuentaRetiro = retirosDirectos.filter((data) => {
-      return data.pk_id_convenio == e.target.Codigo_nit.value && parseInt(data.otp) == parseInt(otp)
-    })
-    if (buscarCuentaRetiro.length < 1) { setOtp(''); setTipoDocumento(''); setDocumento('');alert("Datos Incorrectos"); return false }
-    if (parseInt(monto) <= parseInt(buscarCuentaRetiro[0].monto) ) {
-      alert("Retiro Exitoso")
-      buscarCuentaRetiro[0].monto = buscarCuentaRetiro[0].monto - monto
-      setShowModal(false);
-      setSelected(false)
-    }else {alert("Saldo insuficiente")}
-  }, [selected?.id_convenio, otp, tipoDocumento, documento, monto])
+  const getRetiros = useCallback(async () => {
+    await getRetirosList({ limit: pageData.limit, offset: pageData.page === 1 ? 0 : (pageData.page * pageData.limit) - pageData.limit })
+      .then((data) => { setListRetiros(data.results); setMaxPages(data.maxPages) })
+    setCargando(true)
+  }, [pageData])
 
-  const handleClose = useCallback(() => {
-    setShowModal(false);
-    setSelected(false)
-  }, []);
-  useEffect(() => {
-  if (selected !== '') navigate(`/recaudo-directo/consultar-retiro/retirar/${selected.pk_id_convenio}/${selected.nombre_convenio}`)
-  }, [selected]);
+  useEffect(() => { getRetiros() }, [getRetiros])
 
   return (
     <Fragment>
       <h1 className="text-3xl mt-6">Convenios de Retiros Directos</h1>
+      {cargando ? (
       <TableEnterprise
         title="Convenios de Retiros"
         headers={[
@@ -87,21 +33,22 @@ const RetiroDirecto = () => {
           "Código EAN o IAC",
           "Nombre convenio",
         ]}
-        data={convenioRetiro['value'].map(
+        data={listRetiro.map(
           ({
-            pk_id_convenio,
-            codigo_ean_iac,
+            pk_id_convenio_directo,
+            ean13,
             nombre_convenio,
           }) => ({
-            pk_id_convenio,
-            codigo_ean_iac,
+            pk_id_convenio_directo,
+            ean13,
             nombre_convenio,
           })
         )}
         onSelectRow={(e, i) => {
-          setShowModal(true);
-          setSelected(convenioRetiro['value'][i]);
+          navigate(`/recaudo-directo/consultar-retiro/retirar/${listRetiro[i].pk_id_convenio_directo}/${listRetiro[i].nombre_convenio}`)
         }}
+        maxPage={maxPages}
+        onSetPageData={setPageData}
         onChange={(ev) => {
           setBusqueda(ev)
         }
@@ -149,89 +96,7 @@ const RetiroDirecto = () => {
           required
         />
       </TableEnterprise>
-      {/* <Modal show={showModal} handleClose={handleClose}>
-        <h2 className="text-3xl mx-auto text-center mb-4"> Realizar retiro </h2>
-        <Form onSubmit={consultarRetiroDirecto} grid >
-          <Input
-            id={"Codigo_nit"}
-            label={"Codigo convenio"}
-            name={"Codigo_nit"}
-            autoComplete="off"
-            defaultValue={selected?.pk_id_convenio ?? ""}
-            disabled
-            required />
-          <Input
-            id={"codigo_ean_iac"}
-            label={"Código EAN o IAC"}
-            name={"codigo_ean_iac"}
-            defaultValue={selected?.codigo_ean_iac ?? ""}
-            autoComplete="off"
-            disabled
-          />
-          <Input
-            id={"nombre_convenio"}
-            label={"Nombre convenio"}
-            name={"nombre_convenio"}
-            type="text"
-            defaultValue={selected?.nombre_convenio ?? ""}
-            autoComplete="off"
-            disabled
-            required
-          />
-          <Input
-            id={1}
-            label={"Numero OTP"}
-            name={"OTP"}
-            type="text"
-            minLength={"6"}
-            maxLength={"6"}
-            onChange={(e) => { setOtp(e.target.value) }}
-            autoComplete="off"
-            required />
-          <Select
-            id='tipoDocumento'
-            label='Tipo de documento'
-            options={optionsDocumento}
-            onChange={(e) => { setTipoDocumento(e.target.value) }}
-            required
-          />
-          <Input
-            id={1}
-            label={"Documento Cliente"}
-            name={"documento"}
-            type="text"
-            minLength={"5"}
-            maxLength={"11"}
-            onChange={(e) => { setDocumento(e.target.value) }}
-            autoComplete="off"
-            required />
-          <Input
-            id={1}
-            label={"Monto"}
-            name={"monto"}
-            type="text"
-            minLength={"1"}
-            maxLength={"15"}
-            onChange={(e) => { setMonto(e.target.value) }}
-            autoComplete="off"
-            required />
-          {selected && (
-            <ToggleInput
-              id={"activo"}
-              label={"Se encuentra activo"}
-              name={"activo"}
-              defaultChecked={selected?.activo}
-              disabled
-            />
-          )}
-          <ButtonBar>
-            <Button type={"submit"} >
-              Retirar
-            </Button>
-          </ButtonBar>
-        </Form>
-      </Modal> */}
-
+      ) : (<>cargando...</>)}
     </Fragment>
   )
 }
