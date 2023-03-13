dbafc81f93054c24c9585bc2940c77816b16f800
Hector Melgarejo <hector.agudelo@cumplimos.co>
2023-03-13 18:13:33 -0500

    Longitud Documento consulta pines

--- src/apps/PinesVus/Views/TramitePines.jsx
+++ src/apps/PinesVus/Views/TramitePines.jsx
@@ -299,14 +299,15 @@ const TramitePines = () => {
               <Input
                 id="paramBusqueda"
                 label="Documento"//"Código"
-                type="number"
-                minLength="10"
-                maxLength="10"
+                type="text"
+                minLength="12"
+                maxLength="5"
                 autoComplete="off"
                 value={parametroBusqueda}
                 required
                 onInput={(e) => {
-                  setParametroBusqueda(e.target.value);
+                  const num = parseInt(e.target.value) || "";
+                  setParametroBusqueda(num);
                 }}
               />
               <ButtonBar className="col-auto md:col-span-2">
