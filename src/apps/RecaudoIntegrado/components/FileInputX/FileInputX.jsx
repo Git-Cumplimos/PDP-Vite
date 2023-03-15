c28dca8ab4c2c0cb361df2c97cec9d98227ff23c
cristhiancao9 <u1802342@unimilitar.edu.co>
2023-03-15 13:29:49 -0500

    cambios en servicio de contingencia

--- src/apps/RecaudoIntegrado/components/FileInputX/FileInputX.jsx
+++ src/apps/RecaudoIntegrado/components/FileInputX/FileInputX.jsx
@@ -2,12 +2,14 @@ import Button from "../../../../components/Base/Button";
 import FileInput from "../../../../components/Base/FileInput";
 import classes from "./FileInputX.module.css";
 import { useCallback, useState } from "react";
-import { Presigned } from "../../utils/fetchBucket";
-import { notifyError } from "../../../../utils/notify";
+import { Presigned, Presigned_validar } from "../../utils/fetchBucket";
+import { notify, notifyError, notifyPending } from "../../../../utils/notify";
+import { Validar_archivo } from "../../utils/fetchValidarArchivo";
+import SimpleLoading from "../../../../components/Base/SimpleLoading";
 
 const FileInputX = ({ banco }) => {
   const banco_minuscula = banco.charAt(0).toLowerCase() + banco.slice(1);
-  console.log(banco_minuscula);
+  // console.log(banco_minuscula);
   const urlAssets = process.env.REACT_APP_ASSETS_URL;
   const {
     contendorPrincipalFormulario,
@@ -18,27 +20,55 @@ const FileInputX = ({ banco }) => {
     contenedorArchivos,
     contenedorBtns,
     nombreArchivo,
+    nombreArchivoStyle,
     fileInput,
     contenedorLabel,
     btnBasura,
     divsuperior,
     divinferior,
+    nombreLabel,
     contenedorArchivosBasura,
   } = classes;
 
   const [nombreDocumento, setNombreDocumento] = useState("");
   const [archivo, setArchivo] = useState([]);
   const [disabledBtn, setDisabledBtn] = useState(true);
+  const [procesandoValidacion, setProcesandoValidacion] = useState(false);
+  const [type1, setType1] = useState([]);
 
   //------------------Guardar Archivos PDF---------------------//
-  const onFileChange = useCallback((files) => {
-    if (Array.isArray(Array.from(files))) {
-      files = Array.from(files);
-      setArchivo(files);
-      setNombreDocumento(files[0]?.name);
-      setDisabledBtn(false);
-    }
-  }, []);
+  const onFileChange = useCallback(
+    (files) => {
+      if (Array.isArray(Array.from(files))) {
+        files = Array.from(files);
+        setArchivo(files);
+        setNombreDocumento(files[0]?.name);
+        setDisabledBtn(false);
+        const tipo_archivo = files[0]?.type.toString();
+        console.log(tipo_archivo);
+        // console.log(!/^.*\.fil$/.test(files[0].name));
+        if (tipo_archivo !== "text/csv" && banco_minuscula === "davivienda") {
+          // Muestra una notificación si la extensión del archivo es diferente de ".fil" o ".csv"
+          document.getElementById("contingencia").value = ""; // <- limpia el valor del campo de archivo
+          setArchivo([]);
+          setNombreDocumento("");
+          notifyError("La extensión del archivo debe ser de tipo text/csv");
+          return;
+        } else if (
+          tipo_archivo !== "text/plain" &&
+          !/^.*\.fil$/.test(files[0].name) &&
+          banco_minuscula === "bancolombia"
+        ) {
+          document.getElementById("contingencia").value = ""; // <- limpia el valor del campo de archivo
+          setArchivo([]);
+          setNombreDocumento("");
+          notifyError("La extensión del archivo debe ser .fil");
+          return;
+        }
+      }
+    },
+    [banco_minuscula]
+  );
   const EliminarDocumento = (e) => {
     e.preventDefault();
     document.getElementById("contingencia").value = ""; // <- limpia el valor del campo de archivo
@@ -47,28 +77,157 @@ const FileInputX = ({ banco }) => {
     setDisabledBtn(true);
   };
 
-  const EnviarArchivos = (e) => {
+  const EnviarArchivos = async (e) => {
     e.preventDefault();
     setDisabledBtn(true);
-    if (archivo[0]) {
-      console.log("ARCHIVO[0]", archivo[0]);
-      Presigned(archivo[0], banco_minuscula)
-        .then((res) => {
-          setArchivo([]);
-          setNombreDocumento("");
-          setDisabledBtn(false);
-        })
-        .catch((err) => console.error(err));
-    } else {
-      if (!archivo[0]) {
-        notifyError("Por favor adjunte el archivo de contingencia.");
+
+    if (!archivo[0]) {
+      notifyError("Por favor adjunte el archivo de contingencia.");
+      setDisabledBtn(false);
+      return;
+    }
+
+    try {
+      setProcesandoValidacion(true);
+      const presignedData = await Presigned_validar(
+        archivo[0],
+        banco_minuscula
+      );
+      const nombreArchivoS3 = presignedData.nombre_archivo;
+
+      if (!nombreArchivoS3) {
+        notifyError("Error, no se recibio el archivo.");
+        setProcesandoValidacion(false);
+        document.getElementById("contingencia").value = ""; // <- limpia el valor del campo de archivo
+        setArchivo([]);
+        setNombreDocumento("");
         setDisabledBtn(false);
+        return;
       }
+
+      Validar_archivo(banco_minuscula, nombreArchivoS3).then((res) => {
+        console.log(res);
+
+        setProcesandoValidacion(false);
+        document.getElementById("contingencia").value = ""; // <- limpia el valor del campo de archivo
+        setProcesandoValidacion(false);
+        setArchivo([]);
+        setNombreDocumento("");
+        setDisabledBtn(false);
+      });
+    } catch (error) {
+      console.error(error);
+      document.getElementById("contingencia").value = ""; // <- limpia el valor del campo de archivo
+      setDisabledBtn(false);
     }
   };
 
+  // const EnviarArchivos = async (e) => {
+  //   e.preventDefault();
+  //   setDisabledBtn(true);
+
+  //   if (!archivo[0]) {
+  //     notifyError("Por favor adjunte el archivo de contingencia.");
+  //     setDisabledBtn(false);
+  //     return;
+  //   }
+
+  //   try {
+  //     setProcesandoValidacion(true);
+  //     const presignedData = await Presigned_validar(
+  //       archivo[0],
+  //       banco_minuscula
+  //     );
+  //     const nombreArchivoS3 = presignedData.nombre_archivo;
+
+  //     if (!nombreArchivoS3) {
+  //       notifyError("Error, no se recibio el archivo.");
+  //       setProcesandoValidacion(false);
+  //       setArchivo([]);
+  //       setNombreDocumento("");
+  //       setDisabledBtn(false);
+  //       return;
+  //     }
+
+  //     const res = await Validar_archivo(banco_minuscula, nombreArchivoS3);
+
+  //     setProcesandoValidacion(false);
+  //     notifyPending(
+  //       Promise.resolve(res),
+  //       {},
+  //       { render: ({ data }) => `Success ${JSON.stringify(data)}` },
+  //       { render: ({ data }) => `Error ${data.message}` }
+  //     );
+
+  //     setArchivo([]);
+  //     setNombreDocumento("");
+  //     setDisabledBtn(false);
+  //   } catch (error) {
+  //     console.error(error);
+  //     setDisabledBtn(false);
+  //   }
+  // };
+
+  // const EnviarArchivos = (e) => {
+  //   e.preventDefault();
+  //   setDisabledBtn(true);
+  //   if (archivo[0]) {
+  //     console.log("ARCHIVO[0]", archivo[0]);
+  //     setProcesandoValidacion(true);
+  //     Presigned_validar(archivo[0], banco_minuscula)
+  //       .then((res) => {
+  //         setNombreArchivoS3(res?.nombre_archivo);
+
+  //         if (res?.nombre_archivo) {
+  //           notifyPending(
+  //             new Promise((resolve, reject) => {
+  //               setTimeout(() => {
+  //                 Validar_archivo(banco_minuscula, res?.nombre_archivo)
+  //                   .then((res) => {
+  //                     setProcesandoValidacion(false);
+  //                     resolve(res);
+  //                   })
+  //                   .catch((err) => {
+  //                     setProcesandoValidacion(false);
+  //                     reject(err);
+  //                   });
+  //               }, 3000);
+  //             }),
+  //             {},
+  //             { render: ({ data }) => `Success ${JSON.stringify(data)}` },
+  //             { render: ({ data }) => `Error ${data.message}` },
+
+  //           );
+  //           // setTimeout(
+  //           //   () =>
+  //           //     Validar_archivo(banco_minuscula, res?.nombre_archivo).then(
+  //           //       (res) => {
+  //           //         setProcesandoValidacion(false);
+  //           //         notify(res);
+  //           //       }
+  //           //     ),
+  //           //   3000
+  //           // );
+  //         } else {
+  //           notifyError("Error, no se recibio el archivo.");
+  //           setProcesandoValidacion(false);
+  //         }
+  //         setArchivo([]);
+  //         setNombreDocumento("");
+  //         setDisabledBtn(false);
+  //       })
+  //       .catch((err) => console.error(err));
+  //   } else {
+  //     if (!archivo[0]) {
+  //       notifyError("Por favor adjunte el archivo de contingencia.");
+  //       setDisabledBtn(false);
+  //     }
+  //   }
+  // };
+
   return (
     <div className={contendorPrincipalFormulario}>
+      <SimpleLoading show={procesandoValidacion}></SimpleLoading>
       <form onSubmit={(e) => EnviarArchivos(e)} className={contenedorForm}>
         <div className={contenedorInput}>
           <h2 className={titulo}>
@@ -83,7 +242,7 @@ const FileInputX = ({ banco }) => {
                   label={" "}
                   className={fileInput}
                   onGetFile={onFileChange}
-                  accept=".xlsx"
+                  accept=".fil, .csv"
                   allowDrop={false}
                 ></FileInput>
                 <div className={divsuperior}></div>
@@ -94,7 +253,9 @@ const FileInputX = ({ banco }) => {
           </div>
           {archivo[0] ? (
             <label className={contenedorArchivosBasura}>
-              <div className={nombreArchivo}> {nombreDocumento} </div>
+              <div className={nombreArchivoStyle}>
+                <h5 className={nombreLabel}>{nombreDocumento}</h5>
+              </div>
 
               <button
                 className={btnBasura}
