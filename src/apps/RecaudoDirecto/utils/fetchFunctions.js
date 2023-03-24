7d9b60720d8e977cd27ce9d1c20daf1c05184158
KevinG3511 <kevin.guevara@cumplimos.co>
2023-03-24 09:18:06 -0500

    fix: correccion de rutas

--- src/apps/RecaudoDirecto/utils/fetchFunctions.js
+++ src/apps/RecaudoDirecto/utils/fetchFunctions.js
@@ -1,7 +1,7 @@
 import fetchData from "../../../utils/fetchData";
 
-const url = `http://127.0.0.1:8000`;
-// const url = `${process.env.REACT_APP_URL_RECAUDO_RETIRO_DIRECTO}`;
+// const url = `http://127.0.0.1:8000`;
+const url = `${process.env.REACT_APP_URL_RECAUDO_RETIRO_DIRECTO}`;
 
 const buildGetFunction = (url) => {
   return async (args = {}) => {
