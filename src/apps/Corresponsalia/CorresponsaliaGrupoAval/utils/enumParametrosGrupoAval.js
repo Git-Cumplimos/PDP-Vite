09f8a08aac46706169927248cf925103e09d7a82
Hector Melgarejo <hector.agudelo@cumplimos.co>
2023-04-12 14:43:02 -0500

    Pruabas aval monto minimo de depósito en 1

--- src/apps/Corresponsalia/CorresponsaliaGrupoAval/utils/enumParametrosGrupoAval.js
+++ src/apps/Corresponsalia/CorresponsaliaGrupoAval/utils/enumParametrosGrupoAval.js
@@ -1,6 +1,6 @@
 export const enumParametrosGrupoAval = Object.freeze({
     maxDepositoCuentas : 10000000,
-    minDepositoCuentas : 5000,
+    minDepositoCuentas : 1,
     maxRetiroCuentas : 3000000,
     minRetiroCuentas : 5000,
 
