1a09b64852b04c70fb983312ceb69be77fc60795
Alvaro Casas Quintero <alvaro.casas@cumplimos.co>
2023-04-13 16:42:24 -0500

    Fix: buffer

--- craco.config.js
+++ craco.config.js
@@ -9,6 +9,7 @@ module.exports = {
       resolve: {
         fallback: {
           stream: require.resolve("stream-browserify"),
+          buffer: require.resolve("buffer"),
           // crypto: require.resolve("crypto-browserify"),
           // stream: false,
           crypto: false,
