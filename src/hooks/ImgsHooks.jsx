048a6100124e4c40b22dbf04acf8ef2f022a9e85
Nicholas David <nicholas.carreno@cumplimos.co>
2023-04-25 11:25:48 -0500

    refactor: check if banner exists with img request

--- src/hooks/ImgsHooks.jsx
+++ src/hooks/ImgsHooks.jsx
@@ -10,10 +10,13 @@ const initialImgs = {
 
 const FETCH_IMGS = "FETCH_IMGS";
 const SET_IMGS = "SET_IMGS";
+const DELETE_IMGS = "DELETE_IMGS";
 const FETCH_SVGS = "FETCH_SVGS";
 const SET_SVGS = "SET_SVGS";
+const DELETE_SVGS = "DELETE_SVGS";
 const FETCH_BANNERS = "FETCH_BANNERS";
 const SET_BANNERS = "SET_BANNERS";
+const DELETE_BANNERS = "DELETE_BANNERS";
 
 const reducerImgs = (state, action) => {
   const { type: _type, payload } = action;
@@ -21,14 +24,38 @@ const reducerImgs = (state, action) => {
     case SET_IMGS:
       return { ...state, imgs: { ...state.imgs, [payload.name]: payload.img } };
 
+    case DELETE_IMGS:
+      const newImgs = structuredClone(state.imgs);
+      delete newImgs[payload.name];
+      return {
+        ...state,
+        banners: newImgs,
+      };
+
     case SET_BANNERS:
       return {
         ...state,
-        banners: payload,
+        banners: { ...state.banners, [payload.name]: payload.banner },
+      };
+
+    case DELETE_BANNERS:
+      const newBanners = structuredClone(state.banners);
+      delete newBanners[payload.name];
+      return {
+        ...state,
+        banners: newBanners,
       };
 
     case SET_SVGS:
       return { ...state, svgs: { ...state.svgs, [payload.name]: payload.svg } };
+
+    case DELETE_SVGS:
+      const newSvgs = structuredClone(state.svgs);
+      delete newSvgs[payload.name];
+      return {
+        ...state,
+        banners: newSvgs,
+      };
 
     case FETCH_IMGS:
       for (const [key, val] of Object.entries(images)) {
@@ -40,10 +67,21 @@ const reducerImgs = (state, action) => {
       return state;
 
     case FETCH_BANNERS:
-      payload.dispatch({
-        type: SET_BANNERS,
-        payload: payload.banners,
-      });
+      for (const [key, val] of Object.entries(banners)) {
+        checkImage(val)
+          .then(() => {
+            payload.dispatch({
+              type: SET_BANNERS,
+              payload: { name: key, banner: val },
+            });
+          })
+          .catch(() => {
+            payload.dispatch({
+              type: DELETE_BANNERS,
+              payload: { name: key },
+            });
+          });
+      }
       return state;
 
     case FETCH_SVGS:
@@ -74,36 +112,15 @@ export const useProvideImgs = () => {
   useEffect(() => {
     dispatchImgs({ type: FETCH_IMGS, payload: { dispatch: dispatchImgs } });
     dispatchImgs({ type: FETCH_SVGS, payload: { dispatch: dispatchImgs } });
-    checkBanners(banners).then((data) => {
-      dispatchImgs({
-        type: FETCH_BANNERS,
-        payload: { banners: data, dispatch: dispatchImgs },
-      });
-    });
+    dispatchImgs({ type: FETCH_BANNERS, payload: { dispatch: dispatchImgs } });
   }, []);
   return assets;
 };
 
-const checkBanners = async (banners_data) => {
-  const data_banners = {};
-  for (const [key, val] of Object.entries(banners_data)) {
-    const res = await checkImage(val);
-    if (res) {
-      data_banners[key] = val;
-    }
-  }
-  return data_banners;
-};
-
-const checkImage = async (url) => {
-  try {
-    const response = await fetch(url);
-    if (response.status >= 400) {
-      return false;
-    }
-    return true;
-  } catch (error) {
-    return false;
-  }
-};
-export default checkImage;
+const checkImage = (url) =>
+  new Promise((resolve, reject) => {
+    const load_banners = new Image();
+    load_banners.src = url;
+    load_banners.onload = () => resolve(url);
+    load_banners.onerror = () => reject("Imagen no encontrada");
+  });
