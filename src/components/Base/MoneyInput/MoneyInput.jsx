698c6103310aee3577b2c0944ab422d4b6b793be
Nicholas David <nicholas.carreno@cumplimos.co>
2023-05-03 12:17:16 -0500

    feat: check money nput on load component

--- src/components/Base/MoneyInput/MoneyInput.jsx
+++ src/components/Base/MoneyInput/MoneyInput.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from "react";
+import { useCallback, useEffect, useMemo, useRef } from "react";
 import useMoney from "../../../hooks/useMoney";
 import { makeMoneyFormatter, moneyValidator } from "../../../utils/functions";
 import Input from "../Input";
@@ -6,6 +6,8 @@ import Input from "../Input";
 export const formatMoney = makeMoneyFormatter(2);
 
 const MoneyInput = ({ decimalDigits = 0, equalError = true, ...input }) => {
+  const inptRef = useRef(null);
+
   const inputLimits = useMemo(() => {
     const minVal = parseInt(input?.min) || 0;
     const maxVal = parseInt(input?.max) || 10000000;
@@ -24,6 +26,39 @@ const MoneyInput = ({ decimalDigits = 0, equalError = true, ...input }) => {
   const localFormatMoney = useMemo(
     () => makeMoneyFormatter(decimalDigits),
     [decimalDigits]
+  );
+
+  useEffect(
+    () => {
+      if (inptRef.current) {
+        const moneyFormatter = makeMoneyFormatter(decimalDigits);
+  
+        const moneyValue =
+          Math.round(
+            moneyValidator(inptRef.current.value) * Math.pow(10, decimalDigits)
+          ) / Math.pow(10, decimalDigits);
+  
+        const [min, max] = inputLimits;
+        if (moneyValue < min) {
+          inptRef.current.setCustomValidity(
+            `El valor debe ser mayor a ${moneyFormatter.format(min)}`
+          );
+        } else if (moneyValue > max) {
+          inptRef.current.setCustomValidity(
+            `El valor debe ser menor${
+              !equalError ? " o igual" : ""
+            } a ${moneyFormatter.format(max)}`
+          );
+        } else if (moneyValue === max && equalError) {
+          inptRef.current.setCustomValidity(
+            `El valor debe ser menor a ${moneyFormatter.format(max)}`
+          );
+        } else {
+          inptRef.current.setCustomValidity("");
+        }
+      }
+    },
+    [decimalDigits, equalError, inputLimits]
   );
 
   const onInput = useCallback(
@@ -52,7 +87,7 @@ const MoneyInput = ({ decimalDigits = 0, equalError = true, ...input }) => {
     return Object.fromEntries(_props)
   }, [input?.value, input?.defaultValue, localFormatMoney])
 
-  return <Input {...input} {...dynamicProps} onInput={onInput} />;
+  return <Input {...input} {...dynamicProps} ref={inptRef} onInput={onInput} />;
 };
 
 export default MoneyInput;
