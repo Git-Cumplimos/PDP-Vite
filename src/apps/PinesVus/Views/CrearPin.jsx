fa295234e33918ab7583248e8daf0a6096c77ccf
Hector Melgarejo <hector.agudelo@cumplimos.co>
2023-05-30 07:45:07 -0500

    Agregar pago con datafono

--- src/apps/PinesVus/Views/CrearPin.jsx
+++ src/apps/PinesVus/Views/CrearPin.jsx
@@ -73,6 +73,10 @@ const CrearPin = () => {
   const [firma, setFirma] = useState("")
   const [pedirFirma, setPedirFirma] = useState(true)
   const [descripcionTipDoc, setDescripcionTipDoc] = useState("")
+  const [metodoPago, setMetodoPago] = useState(1)
+  const [codigoPago, setCodigoPago] = useState("")
+  const [codigoPagoVerificacion, setCodigoPagoVerificacion] = useState("")
+
 
   const [olimpia, setOlimpia] = useState("")
 
@@ -495,9 +499,9 @@ const CrearPin = () => {
     }}
     });
   };
-  console.log("HOMELOATION",homeLocation)
   const onSubmit = (e) => {
     e.preventDefault();
+    if (codigoPago === codigoPagoVerificacion){
     setDisabledBtns(true);
     const hora_actual=Intl.DateTimeFormat("es-CO", {
       hour: "numeric",
@@ -534,6 +538,9 @@ const CrearPin = () => {
     objTicket["timeInfo"]["Hora"] = hora;
     objTicket["commerceName"] = pinData["descripcion"]
     objTicket["trxInfo"][0] = ["Trámite", "Creación de Pin"]
+    objTicket["trxInfo"][2] = ["Documento", documento]
+    objTicket["trxInfo"][3] = ["Metodo de pago", metodoPago === 1? 'Efectivo' : 'Tarjeta']
+    objTicket["trxInfo"][4] = ["Cod. Datafono", codigoPago]
     // objTicket["trxInfo"][2] = ["Valor Pin", formatMoney.format(respPin?.valor)]
     // objTicket["trxInfo"][3] = ["IVA Pin",formatMoney.format(respPin?.valor_iva)]
     // objTicket["trxInfo"][4] = ["Total", formatMoney.format(respPin?.valor + respPin?.valor_iva)] 
@@ -601,6 +608,10 @@ const CrearPin = () => {
       })
       .catch(() => setDisabledBtns(false));
     }
+    }
+    else{
+      notifyError("Verifique que el código suministrado por el datafono sea correcto")
+    }
   };
 
   const closeModal = useCallback(async () => {
@@ -662,7 +673,7 @@ const CrearPin = () => {
     }
 
   }, [venderVehiculo,tipoPin, hora, horaCierre, navigate, cierreManual])
-  
+  console.log(metodoPago, metodoPago === 2)
   return (
     <>
     {"id_comercio" in roleInfo ? (
@@ -1281,6 +1292,7 @@ const CrearPin = () => {
                   formatMoney.format(pinData.total + tramiteData.total)}</h1>
                 } 
               </div>
+              <br></br>
             </>
             {/* {Object.entries(tramiteData).map(([key, val]) => {
               return (
@@ -1297,6 +1309,55 @@ const CrearPin = () => {
             })} */}
             <div className="flex flex-col justify-center items-center mx-auto container">
               <Form onSubmit={onSubmit}>
+              <Select
+                id="metodoPago"
+                label="Metodo de pago"
+                required
+                options={[
+                  { value: 1, label: "Efectivo" },
+                  { value: 2, label: "Tarjeta" },
+                ]}
+                value={metodoPago}
+                onChange={(e) => {
+                  setMetodoPago(e.target.value);
+                  setDisabledBtnsContinuar(false)
+                  setCodigoPago("")
+                }}
+              />
+              { metodoPago === '2' ?
+              <>
+              <Input
+              id="codPago"
+              label="Código datafono"
+              type="text"
+              autoComplete="off"
+              value={codigoPago}
+              onInput={(e) => {
+                const num = parseInt(e.target.value) || "";
+                setCodigoPago(num);
+              }}
+              />
+              <Input
+              id="codPagoVerificacion"
+              label="Verificación código"
+              type="text"
+              autoComplete="off"
+              value={codigoPagoVerificacion}
+              onInput={(e) => {
+                if (
+                  (String(e.target.value).length > 2) &
+                  (String(codigoPagoVerificacion).length < 1)
+                ) {
+                  notifyError("Debe digitar el código y no pegarlo");
+                } else {                  
+                  const num = parseInt(e.target.value) || "";
+                  setCodigoPagoVerificacion(num);
+                }
+              }}
+              />
+              </>
+              :
+              ""}
                 <ButtonBar>
                   <Button type="submit" disabled={disabledBtns}>
                     Crear Pin
