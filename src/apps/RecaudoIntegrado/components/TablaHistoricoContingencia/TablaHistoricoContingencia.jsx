57f33c0b26d2ebda9600647a74163ce9c4a93e39
KevinG3511 <kevin.guevara@cumplimos.co>
2023-06-02 10:28:32 -0500

    feat: cambio al general el archivo

--- src/apps/RecaudoIntegrado/components/TablaHistoricoContingencia/TablaHistoricoContingencia.jsx
+++ src/apps/RecaudoIntegrado/components/TablaHistoricoContingencia/TablaHistoricoContingencia.jsx
@@ -52,7 +52,7 @@ const TablaHistoricoContingencia = ({ banco }) => {
 
   const downloadExcel = useCallback(
     (data) => {
-      fetchExcel(`${urlBackend}/generarexcel`, {
+      fetchExcel(`${urlBackend}/descargarexcel`, {
         method: "POST",
         body: JSON.stringify(data),
         headers: { "Content-Type": "application/json" },
@@ -60,6 +60,7 @@ const TablaHistoricoContingencia = ({ banco }) => {
     },
     [fetchExcel]
   );
+
 
   useFetchDebounce(
     useMemo(() => {
@@ -134,17 +135,18 @@ const TablaHistoricoContingencia = ({ banco }) => {
       }
       onSelectRow={(_, i) =>
         downloadExcel({
+          pk_contingencia_recaudo_bancos:datosTablaContingencia[i].pk_contingencia_recaudo_bancos,
           nombre_banco: datosTablaContingencia[i].identificador_banco,
-          fecha_carga: datosTablaContingencia[i].fecha_carga_archivo,
-          total_registros: datosTablaContingencia[i].cantidad_registros,
-          registros_procesados: datosTablaContingencia[i].cantidad_trx_exitosas,
-          registros_fallidos: datosTablaContingencia[i].cantidad_trx_fallidas,
-          respuesta_trx_exitosas: JSON.stringify(
-            datosTablaContingencia[i].respuuestas_trx
-          ),
-          respuesta_trx_fallidas: JSON.stringify(
-            datosTablaContingencia[i].respuestas_trx_fallidas
-          ),
+          // fecha_carga: datosTablaContingencia[i].fecha_carga_archivo,
+          // total_registros: datosTablaContingencia[i].cantidad_registros,
+          // registros_procesados: datosTablaContingencia[i].cantidad_trx_exitosas,
+          // registros_fallidos: datosTablaContingencia[i].cantidad_trx_fallidas,
+          // respuesta_trx_exitosas: JSON.stringify(
+          //   datosTablaContingencia[i].respuuestas_trx
+          // ),
+          // respuesta_trx_fallidas: JSON.stringify(
+          //   datosTablaContingencia[i].respuestas_trx_fallidas
+          // ),
         })
       }
       onSetPageData={setPageData}
