b3851f0a15caea378f514b67dc7901f77ff52174
jjbarajass <jose.barajas@cumplimos.com>
2023-07-31 13:34:35 -0500

    Se corrigen los valores maximos y minimos de pago tarjeta y de cartera

--- src/apps/Corresponsalia/CorresponsaliaBancoAgrario/utils/enumParametrosPagoCartera.js
+++ src/apps/Corresponsalia/CorresponsaliaBancoAgrario/utils/enumParametrosPagoCartera.js
@@ -1,4 +1,6 @@
 export const  enumParametrosPagoCartera = Object.freeze({
   minPagoCarteraTarjCredito: 1,
   maxPagoCarteraTarjCredito: 500000,
+  minPagoCartera: 1,
+  maxPagoCartera: 500000,
 });
