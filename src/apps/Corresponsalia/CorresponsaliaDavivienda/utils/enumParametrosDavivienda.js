c39bf1f007eeff4a9f4f16c2e734cd4fc3654c57
Alvaro Casas Quintero <alvaro.casas@cumplimos.co>
2023-08-10 18:22:14 -0500

    Fix: arreglo de recaudo davivienda

--- src/apps/Corresponsalia/CorresponsaliaDavivienda/utils/enumParametrosDavivienda.js
+++ src/apps/Corresponsalia/CorresponsaliaDavivienda/utils/enumParametrosDavivienda.js
@@ -1,10 +1,12 @@
 export const enumParametrosDavivienda = Object.freeze({
-    maxDepositoCuentas : 9900000,
-    minDepositoCuentas : 1,
-    maxRetiroCuentas : 720000,
-    minRetiroCuentas : 10000,
-    maxCashInDaviplata : 1000000,
-    minCashInDaviplata : 10000,
-
-  });
-
+  maxDepositoCuentas: 9900000,
+  minDepositoCuentas: 1,
+  maxRetiroCuentas: 720000,
+  minRetiroCuentas: 10000,
+  maxCashInDaviplata: 1000000,
+  minCashInDaviplata: 10000,
+  MAXRECAUDOOPERACIONES: 1000000000,
+  MINRECAUDOOPERACIONES: 0,
+  MAXRECAUDO: 9900000,
+  MINRECAUDO: 0,
+});
