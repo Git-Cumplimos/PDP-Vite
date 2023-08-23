efcb0b8b90879e193db9b264d7b9779998f44d23
Hector Melgarejo <hector.agudelo@cumplimos.co>
2023-08-22 23:17:36 -0500

    cambio fk_id_convenio en prod

--- src/apps/PinesCrc/Views/Pines/VentaPines.jsx
+++ src/apps/PinesCrc/Views/Pines/VentaPines.jsx
@@ -621,7 +621,7 @@ const VentaPines = () => {
     if (canal == "1") {
       return{
     "codigo_pin": "0807", 
-    "fk_id_convenio": 2310,//2041, 
+    "fk_id_convenio": 1313,//2041, 
     "fk_tipo_valor": 1, 
     "nombre_convenio": "Venta pines CRC", 
     "permite_modificar": true, 
@@ -634,7 +634,7 @@ const VentaPines = () => {
   };}
   else{ return{
     "codigo_pin": "0043", 
-    "fk_id_convenio": 2310,//2041, 
+    "fk_id_convenio": 1313,//2041, 
     "fk_tipo_valor": 1, 
     "nombre_convenio": "Venta pines CRC", 
     "permite_modificar": false, 
