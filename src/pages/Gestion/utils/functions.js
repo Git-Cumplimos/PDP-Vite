ea934a9642b9d33f867f5b0a4899b36f96b4df1b
KevinG3511 <kevin.guevara@cumplimos.co>
2023-10-02 14:56:49 -0500

    fix: se separa los limites a causa de los meses

--- src/pages/Gestion/utils/functions.js
+++ src/pages/Gestion/utils/functions.js
@@ -6,9 +6,10 @@ export const validateDates = (searchDate) => {
     let limitMax = new Date()
     limitMin.setDate(limitMin.getDate() - 7)
     limitMax.setDate(limitMax.getDate() + 1)
-    let month = String(limitMin.getMonth()+1).length > 2 ? limitMin.getMonth()+1 : `0${limitMin.getMonth()+1}`
-    limitMin = `${limitMin.getFullYear()}-${month}-${limitMin.getDate()}`
-    limitMax = `${limitMax.getFullYear()}-${month}-${limitMax.getDate()}`
+    let monthMin = String(limitMin.getMonth()+1).length >= 2 ? limitMin.getMonth()+1 : `0${limitMin.getMonth()+1}`
+    let monthMax = String(limitMax.getMonth()+1).length >= 2 ? limitMax.getMonth()+1 : `0${limitMax.getMonth()+1}`
+    limitMin = `${limitMin.getFullYear()}-${monthMin}-${limitMin.getDate()}`
+    limitMax = `${limitMax.getFullYear()}-${monthMax}-${limitMax.getDate()}`
     let error = false
     if (searchDate >= limitMax){error = true;notifyError("La fecha máxima es el día actual")}
     if (searchDate < limitMin) {error = true;notifyError("La fecha mínima es una semana atrás")}
