309a1b9a4d9b46a7211190431a0634a2a4c7478b
KevinG3511 <kevin.guevara@cumplimos.co>
2023-10-02 16:43:03 -0500

    fix: validar longitud de las fechas

--- src/pages/Gestion/utils/functions.js
+++ src/pages/Gestion/utils/functions.js
@@ -1,18 +1,27 @@
 import { notifyError } from "../../../utils/notify";
 
-export const validateDates = (searchDate) => {
-    
-    let limitMin = new Date()
-    let limitMax = new Date()
-    limitMin.setDate(limitMin.getDate() - 7)
-    limitMax.setDate(limitMax.getDate() + 1)
-    let monthMin = String(limitMin.getMonth()+1).length >= 2 ? limitMin.getMonth()+1 : `0${limitMin.getMonth()+1}`
-    let monthMax = String(limitMax.getMonth()+1).length >= 2 ? limitMax.getMonth()+1 : `0${limitMax.getMonth()+1}`
-    limitMin = `${limitMin.getFullYear()}-${monthMin}-${limitMin.getDate()}`
-    limitMax = `${limitMax.getFullYear()}-${monthMax}-${limitMax.getDate()}`
-    let error = false
-    if (searchDate >= limitMax){error = true;notifyError("La fecha máxima es el día actual")}
-    if (searchDate < limitMin) {error = true;notifyError("La fecha mínima es una semana atrás")}
+const transformData = (data) => {
+  if (String(data).length >= 2) return String(data)
+  else return `0${String(data)}`
+}
 
-    return error ? false:true
+export const validateDates = (searchDate) => {
+  
+  let limitMin = new Date()
+  let limitMax = new Date()
+  limitMin.setDate(limitMin.getDate() - 7)
+  limitMax.setDate(limitMax.getDate() + 1)
+  limitMin = `${limitMin.getFullYear()}-${transformData(limitMin.getMonth() + 1)}-${transformData(limitMin.getDate())}`
+  limitMax = `${limitMax.getFullYear()}-${transformData(limitMax.getMonth() + 1)}-${transformData(limitMax.getDate())}`
+  let error = false
+  if (searchDate >= limitMax) {
+    error = true;
+    notifyError("La fecha máxima es el día actual") 
   }
+  if (searchDate < limitMin) { 
+    error = true; 
+    notifyError("La fecha mínima es una semana atrás") 
+  }
+
+  return error ? false : true
+}
