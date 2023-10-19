f30da3c9d64d80c6573ab244f28abb17f7e759c1
Nicholas David <nicholas.carreno@cumplimos.co>
2023-10-19 13:26:28 -0500

    Revert "test: img deployed"
    This reverts commit f50fe33a755fd192afbca32635550c5174021c93.

--- src/pages/InformacionGeneral/index.jsx
+++ src/pages/InformacionGeneral/index.jsx
@@ -1,35 +1,14 @@
-import { Fragment, useCallback, useState } from "react";
 import HNavbar from "../../components/Base/HNavbar";
 import { useUrls } from "../../hooks/UrlsHooks";
 import Error403 from "../Error403";
-import FileInput from "../../components/Base/FileInput";
 
 const InformacionGeneral = () => {
   const { urlsInformacionGeneral } = useUrls();
-  const [imgGet, setImgGet] = useState("");
-
-  const functFiles = useCallback((files) => {
-    console.log(files);
-    setImgGet(URL.createObjectURL(files[0]));
-  }, []);
 
   if (!urlsInformacionGeneral.length) {
     return <Error403 />;
   }
-  return (
-    <Fragment>
-      <HNavbar links={urlsInformacionGeneral} isIcon />
-      <br />
-      <br />
-      <FileInput
-        name="aver"
-        id="123_file"
-        label={"Cargar"}
-        onGetFile={functFiles}
-      />
-      <img src={imgGet} alt="no img" />
-    </Fragment>
-  );
+  return <HNavbar links={urlsInformacionGeneral} isIcon />;
 };
 
 export default InformacionGeneral;
