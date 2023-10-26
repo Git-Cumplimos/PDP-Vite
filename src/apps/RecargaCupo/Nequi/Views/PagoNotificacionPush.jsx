480088ea19f95ab336b8cf2101a705372421f3eb
PaolaGuerrero1107 <jenny.guerrero@cumplimos.co>
2023-10-26 10:24:30 -0500

    cambio aplicacion impuesto al costo trx recarga cupo nequi y nombre en modal

--- src/apps/RecargaCupo/Nequi/Views/PagoNotificacionPush.jsx
+++ src/apps/RecargaCupo/Nequi/Views/PagoNotificacionPush.jsx
@@ -64,7 +64,7 @@ const PagoNotificacionPush = () => {
   const onSubmitDeposito = useCallback(
     (ev) => {
       ev.preventDefault();
-      const totalImpuesto = impuesto === 0 ? 0 : ((impuesto * parseInt(valor)) / 100);
+      const totalImpuesto = impuesto === 0 ? 0 : ((impuesto * costoTrx) / 100);
       const costoFinalTrx = costoTrx + parseInt(totalImpuesto)
       const totalRecarga = parseInt(valor) - parseInt(costoFinalTrx)
       if (totalRecarga <= 0){
@@ -75,7 +75,7 @@ const PagoNotificacionPush = () => {
         "Número Nequi": numeroTelefono,
         "Valor de la recarga": formatMoney.format(valor),
         "Costo de transacción": formatMoney.format(costoFinalTrx),
-        "Valor total a recargar": formatMoney.format(totalRecarga),
+        "Valor neto a recargar": formatMoney.format(totalRecarga),
       };
       setCostoTotalTrx(costoFinalTrx)
       setSummary(summary);
