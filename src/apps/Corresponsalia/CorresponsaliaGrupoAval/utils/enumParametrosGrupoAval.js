8b681cad392477ec80b532301dd471aede9e0c92
Alvaro Casas Quintero <alvaro.casas@cumplimos.co>
2023-11-03 15:40:32 -0500

    Aumentar minimos trx

--- src/apps/Corresponsalia/CorresponsaliaGrupoAval/utils/enumParametrosGrupoAval.js
+++ src/apps/Corresponsalia/CorresponsaliaGrupoAval/utils/enumParametrosGrupoAval.js
@@ -8,5 +8,5 @@ export const enumParametrosGrupoAval = Object.freeze({
   MAX_RETIRO_OTP_DALE: 600000,
   MIN_RETIRO_OTP_DALE: 10000,
   MAX_RECAUDO_PILA: 9900000,
-  MIN_RECAUDO_PILA: 1,
+  MIN_RECAUDO_PILA: 1000,
 });
