8e1107946f3914ec645fc08ba8158a905774b473
Alvaro Casas Quintero <alvaro.casas@cumplimos.co>
2023-11-07 15:46:24 -0500

    Feat: mover gestiones de convenios fuera de corresponsalia

--- src/apps/Corresponsalia/CorresponsaliaGrupoAval/enumPermisosAval.js
+++ src/apps/Corresponsalia/CorresponsaliaGrupoAval/enumPermisosAval.js
@@ -4,8 +4,11 @@ export const enumPermisosAval = Object.freeze({
   aval_cb_pago_terceros: 15002,
   aval_cb_pago_subsidios: 15003,
   aval_cb_recaudo: 15004,
-  aval_cb_convenios_recaudo: 15005,
   RETIRO_OTP_DALE: 15006,
   DEPOSITO_DALE: 15008,
   RECAUDO_PILA: 15007,
+});
+
+export const enumPermisosAvalGestion = Object.freeze({
+  aval_cb_convenios_recaudo: 15005,
 });
