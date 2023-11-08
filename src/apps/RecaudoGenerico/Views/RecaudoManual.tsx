a945cdf6a697f3d37881e922da94ded079e1ef7b
AlissonGCumplimos <alisson.garay@cumplimos.co>
2023-11-08 14:56:29 -0500

    arreglo tableEnterprise

--- src/apps/RecaudoGenerico/Views/RecaudoManual.tsx
+++ src/apps/RecaudoGenerico/Views/RecaudoManual.tsx
@@ -1,49 +1,50 @@
-import {Fragment, useCallback, useEffect, useState } from "react";
+import { Fragment, useCallback, useEffect, useState } from "react";
 
 import TableEnterprise from "../../../components/Base/TableEnterprise/TableEnterprise";
 import Input from "../../../components/Base/Input";
+import ButtonBar from "../../../components/Base/ButtonBar";
 
-const RecaudoManual = () => {  
+const RecaudoManual = () => {
+  return (
+    <div className="py-10 flex items-center flex-col">
+      <TableEnterprise
+        title="Recaudo Servicios Públicos y Privados Manual"
+        headers={["Código de convenio", "Nombre de convenio", "EAN"]}
+        data={[]}
+        maxPage={1}
+        onSetPageData={() => {}}
+        onSelectRow={() => {}}
+        onChange={() => {}}
+      >
+        <></>
+      </TableEnterprise>
+      <Input
+        id={"id_convenio"}
+        label={"Código convenio - Nura"}
+        type="tel"
+        autoComplete="off"
+        maxLength={20}
+        onChange={undefined}
+        required
+      />
+      <Input
+        id={"nombre_convenio"}
+        label={"Nombre convenio"}
+        type="text"
+        autoComplete="off"
+        maxLength={30}
+        required
+      />
+      <Input
+        id={"ean"}
+        label={"EAN"}
+        type="text"
+        autoComplete="off"
+        maxLength={13}
+        required
+      />
+    </div>
+  );
+};
 
-    return (
-        <div className="py-10 flex items-center flex-col">
-            <TableEnterprise
-                title="Recaudo Servicios Públicos y Privados Manual"
-                headers={["Código de convenio", "Nombre de convenio", "EAN"]}
-                data={[]}
-                maxPage={1}
-                onSetPageData={()=>{}}
-                onSelectRow={()=>{}}
-                onChange={()=>{}}
-            ></TableEnterprise>
-                <Input
-                    id={"id_convenio"}
-                    label={"Código convenio - Nura"}
-                    type="tel"
-                    autoComplete="off"
-                    maxLength={20}
-                    onChange={undefined}
-                    required
-                />
-                <Input
-                    id={"nombre_convenio"}
-                    label={"Nombre convenio"}
-                    type="text"
-                    autoComplete="off"
-                    maxLength={30}
-                    required
-                />
-                <Input
-                    id={"ean"}
-                    label={"EAN"}
-                    type="text"
-                    autoComplete="off"
-                    maxLength={13}
-                    required
-                />
-            
-      </div>
-    );
-  };
-  
-  export default RecaudoManual;
+export default RecaudoManual;
