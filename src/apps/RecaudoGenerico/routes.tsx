7247ec3267bbc6333f178947606180743afe5eb9
LisbethNunez13 <lisbeth.nunez@cumplimos.co>
2023-11-08 14:38:33 -0500

    recaudo genérico bosquejo

---
+++ src/apps/RecaudoGenerico/routes.tsx
@@ -0,0 +1,35 @@
+import { lazy } from "react";
+
+import AppIcons from "../../components/Base/AppIcons/AppIcons";
+import HNavbar from "../../components/Base/HNavbar/HNavbar";
+import { TypingRoutes } from "../../utils/TypingUtils";
+
+const RecaudoCodigo = lazy(() => import("./Views/RecaudoCodigo"));
+const RecaudoManual = lazy(() => import("./Views/RecaudoManual"));
+
+const routesRecaudoGenerico = {
+  link: "/recaudo-generico",
+  label: <AppIcons Logo={"RECAUDO"} name="Recaudo Servicio Públicos y Privador Genérico" />,
+  component: ({ subRoutes }: { subRoutes: TypingRoutes[] }) => (
+    <HNavbar links={subRoutes} />
+  ),
+  permission: [200, 201, 202],
+  subRoutes: [
+    {
+        link: "/recaudo-generico/manual",
+        label: <AppIcons Logo={"RECAUDO_MANUAL"} name="Recaudo Manual" />,
+          component: RecaudoManual,
+        permission: [200],
+        subRoutes: [],
+    },
+    {
+        link: "/recaudo-generico/codigo",
+        label: <AppIcons Logo={"RECAUDO_CODIGO_DE_BARRAS"} name="Recaudo Código de Barras" />,
+        component: RecaudoCodigo,
+        permission: [200],
+        subRoutes: [],
+    },
+  ],
+};
+
+export default routesRecaudoGenerico;
