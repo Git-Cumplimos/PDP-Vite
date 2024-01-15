2904ddfd2224dc3320858a45914f031ae02a531b
Alvaro Casas Quintero <alvaro.casas@cumplimos.co>
2024-01-15 08:03:42 -0500

    Feat: aplicacion de pago de credito

--- src/apps/CreditoFacilPdp/components/FormPagoCreditoPdp.jsx
+++ src/apps/CreditoFacilPdp/components/FormPagoCreditoPdp.jsx
@@ -1,7 +1,7 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import Input from "../../../components/Base/Input";
-import TableEnterprise from "../../../components/Base/TableEnterprise";
 import MoneyInput, { formatMoney } from "../../../components/Base/MoneyInput";
+import { v4 } from "uuid";
 import Form from "../../../components/Base/Form";
 import Fieldset from "../../../components/Base/Fieldset";
 import ButtonBar from "../../../components/Base/ButtonBar";
@@ -10,44 +10,86 @@ import { enumParametrosCreditosPDP } from "../utils/enumParametrosCreditosPdp";
 import { useFetch } from "../../../hooks/useFetch";
 import { fetchCustom } from "../utils/fetchCreditoFacil";
 import { notifyPending } from "../../../utils/notify";
+import { useAuth } from "../../../hooks/AuthHooks";
+import { useReactToPrint } from "react-to-print";
+import Modal from "../../../components/Base/Modal";
+import Tickets from "../../../components/Base/Tickets";
+import PaymentSummary from "../../../components/Compound/PaymentSummary";
 
 const URL_PAGO_CREDITO = `http://127.0.0.1:5000/pago-credito-facil/consulta-credito`;
-// const URL_CONSULTA_CREDITO = `${process.env.REACT_APP_URL_CORRESPONSALIA_OTROS}/pago-credito-facil/consulta-credito`;
+// const URL_PAGO_CREDITO = `${process.env.REACT_APP_URL_CORRESPONSALIA_OTROS}/pago-credito-facil/consulta-credito`;
 
 const FormPagoCreditoPdp = ({ dataCreditoUnique, closeModule }) => {
+  const uniqueId = v4();
   const [dataInput, setDataInput] = useState({
     valor: 0,
     observaciones: "",
   });
+  const [objTicketActual, setObjTicketActual] = useState({});
+  const [showModal, setShowModal] = useState(false);
+  const [estadoPeticion, setEstadoPeticion] = useState(0);
+  const { roleInfo, pdpUser } = useAuth();
+  const printDiv = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => printDiv.current,
+  });
   const [loadingPeticionPagoCredito, peticionPagoCredito] = useFetch(
-    fetchCustom(URL_PAGO_CREDITO, "POST", "Pago credito")
+    fetchCustom(URL_PAGO_CREDITO, "POST", "Pago crédito")
   );
-  const pagoCredito = useCallback(() => {
-    const data = {
-      // id_comercio: roleInfo?.id_comercio,
-      id_comercio: 10166,
-    };
-    notifyPending(
-      peticionPagoCredito({}, data),
-      {
-        render: () => {
-          return "Procesando pago";
+  const pagoCredito = useCallback(
+    (ev) => {
+      ev.preventDefault();
+      const data = {
+        oficina_propia:
+          roleInfo?.tipo_comercio === "OFICINAS PROPIAS" ||
+          roleInfo?.tipo_comercio === "KIOSCO"
+            ? true
+            : false,
+        valor_total_trx: dataInput?.valor,
+        nombre_comercio: roleInfo?.["nombre comercio"],
+        nombre_usuario: pdpUser?.uname ?? "",
+        comercio: {
+          id_comercio: roleInfo?.id_comercio,
+          id_usuario: roleInfo?.id_usuario,
+          id_terminal: roleInfo?.id_dispositivo,
+          id_uuid_trx: uniqueId,
         },
-      },
-      {
-        render: ({ data: res }) => {
-          const dataTemp = res.obj.data;
-          return "Pago satisfactorio";
+        location: {
+          address: roleInfo?.["direccion"],
+          dane_code: roleInfo?.codigo_dane,
+          city: roleInfo?.["ciudad"],
         },
-      },
-      {
-        render: ({ data: error }) => {
-          closeModule();
-          return error?.message ?? "Consulta fallida";
+        pago_credito: {
+          numero_credito: dataCreditoUnique?.Id,
+          observaciones: dataInput?.observaciones,
         },
-      }
-    );
-  }, []);
+      };
+      const dataAditional = {
+        id_uuid_trx: uniqueId,
+      };
+      notifyPending(
+        peticionPagoCredito(data, dataAditional),
+        {
+          render: () => {
+            return "Procesando pago";
+          },
+        },
+        {
+          render: ({ data: res }) => {
+            const dataTemp = res.obj.data;
+            return "Pago satisfactorio";
+          },
+        },
+        {
+          render: ({ data: error }) => {
+            closeModule();
+            return error?.message ?? "Pago fallido";
+          },
+        }
+      );
+    },
+    [dataCreditoUnique, pdpUser, dataInput, roleInfo, uniqueId]
+  );
   const onChangeFormat = useCallback((ev) => {
     let value = ev.target.value;
     setDataInput((old) => {
@@ -63,108 +105,165 @@ const FormPagoCreditoPdp = ({ dataCreditoUnique, closeModule }) => {
   }, []);
 
   return (
-    <Form onSubmit={() => {}} grid>
-      <Fieldset legend="Datos del credito" className="lg:col-span-2">
-        <Input
-          id="Id"
-          name="Id"
-          label={"No. de crédito"}
-          type="text"
-          autoComplete="off"
-          minLength={0}
-          maxLength={20}
-          value={dataCreditoUnique?.Id}
-          onChange={() => {}}
-          disabled
-          required
-        />
-        <Input
-          id="Fechadesembolso"
-          name="Fechadesembolso"
-          label={"Fecha de desembolso"}
-          type="text"
-          autoComplete="off"
-          minLength={0}
-          maxLength={20}
-          value={new Date(
-            dataCreditoUnique?.Fechadesembolso
-          ).toLocaleDateString("es-ES", {
-            day: "2-digit",
-            month: "2-digit",
-            year: "numeric",
-          })}
-          onChange={() => {}}
-          disabled
-          required
-        />
-        <MoneyInput
-          id="Valordesembolso"
-          name="Valordesembolso"
-          label={"Valor del crédito"}
-          type="tel"
-          autoComplete="off"
-          minLength={0}
-          maxLength={20}
-          value={dataCreditoUnique?.Valordesembolso ?? ""}
-          onChange={() => {}}
-          disabled
-          required
-        />
-        <Input
-          id="Estado"
-          name="Estado"
-          label={"Estado"}
-          type="text"
-          autoComplete="off"
-          minLength={0}
-          maxLength={20}
-          value={dataCreditoUnique?.Estado ?? ""}
-          onChange={() => {}}
-          disabled
-          required
-        />
-        <MoneyInput
-          id="valor"
-          name="valor"
-          label={"Valor a pagar"}
-          type="tel"
-          minLength={5}
-          maxLength={10}
-          autoComplete="off"
-          min={enumParametrosCreditosPDP?.MINPAGOCREDITOPDP}
-          max={enumParametrosCreditosPDP?.MAXPAGOCREDITOPDP}
-          value={dataInput?.valor ?? ""}
-          onInput={onChangeFormatNum}
-          disabled={loadingPeticionPagoCredito}
-          required
-        />
-        <Input
-          id="observaciones"
-          name="observaciones"
-          label={"Observaciones"}
-          type="text"
-          autoComplete="off"
-          minLength={0}
-          maxLength={20}
-          value={dataInput?.observaciones ?? ""}
-          onChange={onChangeFormat}
-          disabled={loadingPeticionPagoCredito}
-          required
-        />
-      </Fieldset>
-      <ButtonBar className="lg:col-span-2">
-        <Button
-          type="button"
-          onClick={() => closeModule()}
-          disabled={loadingPeticionPagoCredito}
-        >
-          Cancelar
-        </Button>
-        <Button type="submit" disabled={loadingPeticionPagoCredito}>
-          Realizar pago
-        </Button>
-      </ButtonBar>
-    </Form>
+    <>
+      <Form onSubmit={pagoCredito} grid>
+        <Fieldset legend="Datos del crédito" className="lg:col-span-2">
+          <Input
+            id="Id"
+            name="Id"
+            label={"No. de crédito"}
+            type="text"
+            autoComplete="off"
+            minLength={0}
+            maxLength={20}
+            value={dataCreditoUnique?.Id}
+            onChange={() => {}}
+            disabled
+            required
+          />
+          <Input
+            id="Fechadesembolso"
+            name="Fechadesembolso"
+            label={"Fecha de desembolso"}
+            type="text"
+            autoComplete="off"
+            minLength={0}
+            maxLength={20}
+            value={new Date(
+              dataCreditoUnique?.Fechadesembolso
+            ).toLocaleDateString("es-ES", {
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+            })}
+            onChange={() => {}}
+            disabled
+            required
+          />
+          <MoneyInput
+            id="Valordesembolso"
+            name="Valordesembolso"
+            label={"Valor del crédito"}
+            type="tel"
+            autoComplete="off"
+            minLength={0}
+            maxLength={20}
+            value={dataCreditoUnique?.Valordesembolso ?? ""}
+            onChange={() => {}}
+            disabled
+            required
+          />
+          <Input
+            id="Estado"
+            name="Estado"
+            label={"Estado"}
+            type="text"
+            autoComplete="off"
+            minLength={0}
+            maxLength={20}
+            value={dataCreditoUnique?.Estado ?? ""}
+            onChange={() => {}}
+            disabled
+            required
+          />
+          <MoneyInput
+            id="valor"
+            name="valor"
+            label={"Valor a pagar"}
+            type="tel"
+            minLength={5}
+            maxLength={10}
+            autoComplete="off"
+            min={enumParametrosCreditosPDP?.MINPAGOCREDITOPDP}
+            max={
+              parseInt(dataCreditoUnique?.Valordesembolso) ??
+              enumParametrosCreditosPDP?.MAXPAGOCREDITOPDP
+            }
+            value={dataInput?.valor ?? ""}
+            onInput={onChangeFormatNum}
+            disabled={loadingPeticionPagoCredito}
+            required
+          />
+          <Input
+            id="observaciones"
+            name="observaciones"
+            label={"Observaciones"}
+            type="text"
+            autoComplete="off"
+            minLength={0}
+            maxLength={20}
+            value={dataInput?.observaciones ?? ""}
+            onChange={onChangeFormat}
+            disabled={loadingPeticionPagoCredito}
+            required
+          />
+        </Fieldset>
+        <ButtonBar className="lg:col-span-2">
+          <Button
+            type="button"
+            onClick={() => closeModule()}
+            disabled={loadingPeticionPagoCredito}
+          >
+            Cancelar
+          </Button>
+          <Button type="submit" disabled={loadingPeticionPagoCredito}>
+            Realizar pago
+          </Button>
+        </ButtonBar>
+      </Form>
+      <Modal show={showModal} className="flex align-middle">
+        <>
+          {estadoPeticion === 0 ? (
+            <PaymentSummary
+              title="¿Está seguro de realizar el pago?"
+              subtitle="Resumen de transacción"
+              summaryTrx={{
+                "Número crédito": dataCreditoUnique?.Id,
+                "Valor a pagar": formatMoney.format(dataInput?.valor),
+              }}
+            >
+              <ButtonBar>
+                <Button
+                  onClick={() => {
+                    closeModule();
+                    // notifyError("Transacción cancelada por el usuario");
+                  }}
+                  disabled={loadingPeticionPagoCredito}
+                >
+                  Cancelar
+                </Button>
+                <Button
+                  type="submit"
+                  onClick={pagoCredito}
+                  disabled={loadingPeticionPagoCredito}
+                >
+                  Realizar recarga
+                </Button>
+              </ButtonBar>
+            </PaymentSummary>
+          ) : estadoPeticion === 1 ? (
+            <div className="flex flex-col justify-center items-center">
+              <Tickets ticket={objTicketActual} refPrint={printDiv} />
+              <h2>
+                <ButtonBar>
+                  <Button onClick={handlePrint}>Imprimir</Button>
+                  <Button
+                    type="submit"
+                    onClick={() => {
+                      closeModule();
+                    }}
+                  >
+                    Aceptar
+                  </Button>
+                </ButtonBar>
+              </h2>
+            </div>
+          ) : (
+            <></>
+          )}
+        </>
+      </Modal>
+    </>
   );
 };
 
