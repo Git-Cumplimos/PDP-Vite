ff5e6d8848c78d9db07bf99bd9675c9cfc9fc788
Sebastian Camilo Martínez Triana <109163919+seba5devcmps@users.noreply.github.com>
2024-01-16 11:44:12 -0500

    feature gestion de zonas en comercios

---
+++ src/apps/TrxParams/utils/fetchZonas.js
@@ -0,0 +1,74 @@
+import fetchData from "../../../utils/fetchData";
+
+const urlParametrizacion =
+  process.env.REACT_APP_URL_SERVICIOS_PARAMETRIZACION_SERVICIOS;
+
+export const fetchZonas = async (obj) => {
+  try {
+    const res = await fetchData(
+      `${urlParametrizacion}/zonas/all`,
+      "GET",
+      {},
+      obj
+    );
+    if (res?.status) {
+      return { ...res?.obj };
+    } else {
+      console.error(res?.msg);
+      return { maxPages: 0, results: [] };
+    }
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const postCreateZona = async (obj) => {
+  try {
+    const res = await fetchData(
+      `${urlParametrizacion}/zonas/create`,
+      "POST",
+      {},
+      obj
+    );
+    if (!res?.status) {
+      console.error(res?.msg);
+    }
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const putEditZona = async (obj) => {
+  try {
+    const res = await fetchData(
+      `${urlParametrizacion}/zonas/update`,
+      "PUT",
+      {},
+      obj
+    );
+    if (!res?.status) {
+      console.error(res?.msg);
+    }
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const postDeleteZona = async (obj) => {
+  try {
+    const res = await fetchData(
+      `${urlParametrizacion}/zonas/delete`,
+      "POST",
+      {},
+      obj
+    );
+    if (!res?.status) {
+      console.error(res?.msg);
+    }
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
