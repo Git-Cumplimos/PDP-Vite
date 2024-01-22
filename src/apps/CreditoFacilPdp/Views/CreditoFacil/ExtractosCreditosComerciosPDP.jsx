631fd11e24eecf224238d3ad873869add385520a
Alvaro Casas Quintero <alvaro.casas@cumplimos.co>
2024-01-22 15:52:40 -0500

    Feat: consulta de creditos comercio

---
+++ src/apps/CreditoFacilPdp/Views/CreditoFacil/ExtractosCreditosComerciosPDP.jsx
@@ -0,0 +1,143 @@
+import { useCallback, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { notifyError, notifyPending } from "../../../../utils/notify";
+import TablaCreditos from "../../components/TablaCreditos";
+import { useAuth } from "../../../../hooks/AuthHooks";
+import { useFetch } from "../../../../hooks/useFetch";
+import { fetchCustom } from "../../utils/fetchCreditoFacil";
+import FormPagoCreditoPdp from "../../components/FormPagoCreditoPdp";
+import TablaExtractoCreditos from "../../components/TablaExtractoCreditos";
+
+const URL_CONSULTA_CREDITO = `${process.env.REACT_APP_URL_CORRESPONSALIA_OTROS}/pago-credito-facil/consulta-credito`;
+
+const DATA_CREDITO_UNIQUE_SIIAN_INI = {
+  Agrupacion: "",
+  Calificacion: "",
+  Calificacionactual: "",
+  Codigoasesor: "",
+  Codigore: "",
+  Cuotasmora: 0,
+  Cuotaspagadas: 0,
+  Diasmoraacumulado: 0,
+  Diasmorapromedio: 0,
+  Estado: "",
+  Fechadesembolso: "",
+  Fechadeultimopago: "",
+  Fechavencimientoproximo: "",
+  Formapago: "",
+  Frecuenciapagocapital: "",
+  Frecuenciapagointeres: "",
+  Id: 0,
+  Idsucursal: 0,
+  Idtercero: 0,
+  Nombreasesor: "",
+  Nombrere: "",
+  Numeroprestamo: "",
+  Saldo: 0,
+  Sucursal: "",
+  Tasaprestamo: 0,
+  Terceroprestamo: "",
+  Tipocredito: "",
+  Valorcuotaactual: 0,
+  Valordecuota: 0,
+  Valordesembolso: 0,
+  Valorinteresanticipado: 0,
+  Valorpagototal: 0,
+  Valorpagototalcausado: 0,
+  Valorparaestaraldia: 0,
+};
+
+const ExtractosCreditosComerciosPDP = () => {
+  const navigate = useNavigate();
+  const { roleInfo } = useAuth();
+  const [dataCreditos, setDataCreditos] = useState([]);
+  const [dataCreditoUnique, setDataCreditoUnique] = useState(
+    DATA_CREDITO_UNIQUE_SIIAN_INI
+  );
+  const [estadoProceso, setEstadoProceso] = useState("consulta");
+  const [loadingPeticionConsultaCredito, peticionConsultaCredito] = useFetch(
+    fetchCustom(URL_CONSULTA_CREDITO, "POST", "Consultar credito")
+  );
+  const closeModule = useCallback(() => {
+    setDataCreditoUnique(DATA_CREDITO_UNIQUE_SIIAN_INI);
+    setDataCreditos([]);
+    notifyError("Pago cancelado por el usuario");
+    navigate(-1);
+  }, [navigate]);
+  useEffect(() => {
+    if (!roleInfo || (roleInfo && Object.keys(roleInfo).length === 0)) {
+      navigate("/");
+    } else {
+      fetchComercio();
+    }
+  }, []);
+  const fetchComercio = useCallback(() => {
+    let hasKeys = true;
+    const keys = [
+      "id_comercio",
+      "id_usuario",
+      "tipo_comercio",
+      "id_dispositivo",
+      "ciudad",
+      "direccion",
+    ];
+    for (const key of keys) {
+      if (!(key in roleInfo)) {
+        hasKeys = false;
+        break;
+      }
+    }
+    if (!hasKeys) {
+      notifyError(
+        "El usuario no cuenta con datos de comercio, no se permite la transaccion"
+      );
+      navigate("/");
+    }
+  }, [roleInfo, navigate]);
+  const consultaCredito = useCallback(() => {
+    const data = {
+      id_comercio: roleInfo?.id_comercio,
+    };
+    notifyPending(
+      peticionConsultaCredito({}, data),
+      {
+        render: () => {
+          return "Procesando consulta";
+        },
+      },
+      {
+        render: ({ data: res }) => {
+          const dataTemp = res.obj.data;
+          if (dataTemp.length === 1) {
+            setDataCreditoUnique(dataTemp[0]);
+          }
+          setDataCreditos(res.obj.data ?? []);
+          setEstadoProceso("inicio");
+          return "Consulta satisfactoria";
+        },
+      },
+      {
+        render: ({ data: error }) => {
+          navigate(-1);
+          return error?.message ?? "Consulta fallida";
+        },
+      }
+    );
+  }, [navigate, roleInfo.id_comercio]);
+  useEffect(() => {
+    consultaCredito();
+    return () => {};
+  }, []);
+
+  return (
+    <>
+      <h1 className="text-3xl">Extracto Créditos</h1>
+      <TablaExtractoCreditos
+        dataCreditos={dataCreditos}
+        setDataCreditoUnique={setDataCreditoUnique}
+      />
+    </>
+  );
+};
+
+export default ExtractosCreditosComerciosPDP;
