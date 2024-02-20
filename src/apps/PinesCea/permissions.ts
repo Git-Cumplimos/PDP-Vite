133772d7835ac392f56a2ff0af19455f858224bf
jairohuertas <125684462+jairohuertas@users.noreply.github.com>
2024-02-20 12:50:33 -0500

    cea

---
+++ src/apps/PinesCea/permissions.ts
@@ -0,0 +1,10 @@
+export enum PermissionsPinesCea {
+  gestion = 6002,
+  deposito = 6001,
+  venta_pines = 6003,
+  recaudo = 6004,
+  pago_giro = 6005,
+  pin_pago = 6006,
+  operarPinesVus= 53,
+  administrarPinesVus= 63  
+}
