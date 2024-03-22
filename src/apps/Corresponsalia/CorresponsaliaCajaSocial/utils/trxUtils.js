85d5f73e5d4fb41bb2af95b9e77a7bdc56d7cfef
Alvaro Casas Quintero <alvaro.casas@cumplimos.co>
2024-03-21 22:00:41 -0500

    Feat: trx recaudo servicios caja social

--- src/apps/Corresponsalia/CorresponsaliaCajaSocial/utils/trxUtils.js
+++ src/apps/Corresponsalia/CorresponsaliaCajaSocial/utils/trxUtils.js
@@ -36,19 +36,26 @@ export const algoCheckTCCreditoRotativoCajaSocial = (numeroCuenta) => {
   const numeroCuentaCut = numeroCuenta.slice(0, 15);
   let conteo = 0;
   let sumTotal = 0;
-  const arrayVali = [2, 1, 2];
+  const arrayVali = [2, 1];
   for (let index = 0; index < numeroCuentaCut.length; index++) {
-    if (conteo === 3) conteo = 0;
+    if (conteo === 2) conteo = 0;
     const numeroCuentaIndividual = parseInt(numeroCuentaCut[index]);
     let resultMulti = numeroCuentaIndividual * arrayVali[conteo];
+    console.log("1", resultMulti);
     if (resultMulti > 9) {
-      const firstDigit = resultMulti / 10;
-      const secondDigit = resultMulti - 10 * firstDigit;
+      const firstDigit = parseInt(Math.floor(resultMulti / 10));
+      console.log("2", firstDigit);
+      const secondDigit = parseInt(resultMulti - 10 * firstDigit);
+      console.log("3", secondDigit);
       resultMulti = firstDigit + secondDigit;
     }
+    console.log("4", resultMulti);
     sumTotal += resultMulti;
     conteo += 1;
   }
+  console.log("8", sumTotal);
   const sumTotalResult = sumTotal % 10;
+  console.log("casdsa", sumTotalResult);
+  console.log("ccxzzzz", parseInt(numeroCuenta.slice(-1)));
   return sumTotalResult === parseInt(numeroCuenta.slice(-1));
 };
