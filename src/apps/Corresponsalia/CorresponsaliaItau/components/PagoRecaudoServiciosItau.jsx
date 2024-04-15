c2f3c69e1c23aef1dc475ddd0582461ad9203672
PaolaGuerrero1107 <jenny.guerrero@cumplimos.co>
2024-04-15 11:34:43 -0500

    recaudo itau arreglo valor a pagar

--- src/apps/Corresponsalia/CorresponsaliaItau/components/PagoRecaudoServiciosItau.jsx
+++ src/apps/Corresponsalia/CorresponsaliaItau/components/PagoRecaudoServiciosItau.jsx
@@ -382,27 +382,26 @@ const PagoRecaudoServiciosItau = ({
           )}
           {convenio.consultaweb === "N" &&
             convenio.modvalor_consweb === "S" &&
-            tipoRecaudo ===
-              "codigoBarras"(
-                <MoneyInput
-                  id="valorTrx"
-                  name="valorTrx"
-                  label={"Valor a pagar"}
-                  type="tel"
-                  maxLength={10}
-                  autoComplete="off"
-                  min={enumParametrosItau?.MIN_RECAUDO_SERVICIOS_ITAU}
-                  max={enumParametrosItau?.MAX_RECAUDO_SERVICIOS_ITAU}
-                  value={dataRecaudo?.valorTrx ?? 0}
-                  onInput={onChangeFormatNum}
-                  disabled={
-                    loadingPeticionPagoRecaudo || loadingPeticionConsultaRecaudo
-                  }
-                  required
-                  equalError={false}
-                  equalErrorMin={false}
-                />
-              )}
+            tipoRecaudo === "codigoBarras" && (
+              <MoneyInput
+                id="valorTrx"
+                name="valorTrx"
+                label={"Valor a pagar"}
+                type="tel"
+                maxLength={10}
+                autoComplete="off"
+                min={enumParametrosItau?.MIN_RECAUDO_SERVICIOS_ITAU}
+                max={enumParametrosItau?.MAX_RECAUDO_SERVICIOS_ITAU}
+                value={dataRecaudo?.valorTrx ?? 0}
+                onInput={onChangeFormatNum}
+                disabled={
+                  loadingPeticionPagoRecaudo || loadingPeticionConsultaRecaudo
+                }
+                required
+                equalError={false}
+                equalErrorMin={false}
+              />
+            )}
           {convenio.consultaweb === "N" && tipoRecaudo !== "codigoBarras" && (
             <MoneyInput
               id="valorTrx"
