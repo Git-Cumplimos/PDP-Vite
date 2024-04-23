bfaba668ba097a28bd6b7f7b73ba32d6f606a64d
Alvaro Casas Quintero <alvaro.casas@cumplimos.co>
2024-04-23 09:44:27 -0500

    Redeploy

--- src/apps/Corresponsalia/CorresponsaliaCajaSocial/Views/RecaudoServiciosPublicosPrivados/RecaudoCodBarrasServiciosCodigoBarrasCajaSocial.jsx
+++ src/apps/Corresponsalia/CorresponsaliaCajaSocial/Views/RecaudoServiciosPublicosPrivados/RecaudoCodBarrasServiciosCodigoBarrasCajaSocial.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useFetch } from "../../../../../hooks/useFetch";
 import { fetchCustom } from "../../utils/fetchCajaSocial";
