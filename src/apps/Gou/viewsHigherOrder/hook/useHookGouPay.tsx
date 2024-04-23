45d98c500dfe64ac63ab7c497a7142f3b595fa21
AlissonGCumplimos <alisson.garay@cumplimos.co>
2024-04-23 11:26:30 -0500

    volver atras para lo la url

--- src/apps/Gou/viewsHigherOrder/hook/useHookGouPay.tsx
+++ src/apps/Gou/viewsHigherOrder/hook/useHookGouPay.tsx
@@ -163,7 +163,6 @@ const useHookWithGouPay = (
               ...old,
               id_trx: resCheckUrlProcess.id_trx,
             }));
-            window.open(resCheckUrlProcess.url_process);
             return {
               url_process: resCheckUrlProcess.url_process,
               what_service: function_name,
@@ -444,10 +443,10 @@ const useHookWithGouPay = (
               35
             ),
           ]);
-          // if (response?.what_service === "PeticionCheckUrlProcess") {
-          //   window.open(response.url_process);
-          //   go_url_process = true;
-          // }
+          if (response?.what_service === "PeticionCheckUrlProcess") {
+            window.open(response.url_process);
+            go_url_process = true;
+          }
         } catch (error: any) {
           if (!(error instanceof ErrorCustomFetch)) {
             throw new ErrorCustomUseHookCode(
@@ -483,9 +482,9 @@ const useHookWithGouPay = (
                 retries: 6,
               }),
             ]);
-            // if (response?.what_service === "PeticionCheckUrlProcess") {
-            //   window.open(response.url_process);
-            // }
+            if (response?.what_service === "PeticionCheckUrlProcess") {
+              window.open(response.url_process);
+            }
           }
         } catch (error: any) {
           if (!(error instanceof ErrorCustomFetch)) {
